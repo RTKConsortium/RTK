f89b6980d66b8fc1cb9addf07607f0e9f94ee67d
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2010-08-26 11:59:22 +0200

    Common RTK macros

---
+++ rtkMacro.h
@@ -0,0 +1,17 @@
+#ifndef RTKMACRO_H
+#define RTKMACRO_H
+
+//--------------------------------------------------------------------
+#define DD(a) std::cout << #a " = [ " << a << " ]" << std::endl;
+//--------------------------------------------------------------------
+
+//--------------------------------------------------------------------
+#define GGO(ggo_filename, args_info)                                    \
+  args_info_##ggo_filename args_info;                                   \
+  cmdline_parser_##ggo_filename##2(argc, argv, &args_info, 1, 1, 0);                    \
+  if (args_info.config_given)                                           \
+    cmdline_parser_##ggo_filename##_configfile (args_info.config_arg, &args_info, 0, 0, 1); \
+  else cmdline_parser_##ggo_filename(argc, argv, &args_info);
+//--------------------------------------------------------------------
+
+#endif // RTKMACRO_H
