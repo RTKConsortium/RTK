500c2ab883dce8c9059c5fd772c3a834e218d2e8
U-Simon-PC\Simon <Simon@Simon-PC.(none)>
2010-09-28 19:48:46 +0200

    Account for new light dbf reader

--- rtkelektasynergygeometry.cxx
+++ rtkelektasynergygeometry.cxx
@@ -1,53 +1,35 @@
 #include "rtkThreeDCircularGeometryXMLFile.h"
 #include "rtkelektasynergygeometry_ggo.h"
 
-#include "xbase/xbase.h"
+#include "rtkDbf.h"
 
 #include <itkTimeProbe.h>
 
-//Synergy DB constants
-#define NO_FIELD_ID_IMAGE_IN_IMAGE    short(0)
-#define NO_FIELD_DICOM_UID_IN_IMAGE   short(27)
-#define NO_FIELD_ID_IMAGE_IN_FRAME    short(1)
-#define NO_FIELD_NUM_FRAME            short(3)
-#define NO_FIELD_ANGLE_IN_FRAME       short(6)
-#define NO_FIELD_X_FLEX_IN_FRAME      short(7)
-#define NO_FIELD_Y_FLEX_IN_FRAME      short(8)
-
 std::string GetImageIDFromDicomUID(const args_info_rtkelektasynergygeometry &args_info)
 {
-  //Open image database
-  xbXBase db;
-
-  xbShort rc;
-  xbDbf imageTable(&db);
-  rc = imageTable.OpenDatabase( args_info.image_db_arg );
-  if(rc!=XB_NO_ERROR){
+  // Open image database file
+  rtk::DbfFile dbImage(args_info.image_db_arg);
+  if (!dbImage.is_open()){
     std::cerr << "Couldn't open " << args_info.image_db_arg << std::endl;
     exit(EXIT_FAILURE);
   }
-  rc = imageTable.GetFirstRecord();
-  if(rc!=XB_NO_ERROR)
+
+  // Search for correct record
+  bool bReadOk;
+  do {
+    bReadOk = dbImage.ReadNextRecord();
+  }
+  while(bReadOk && std::string(args_info.dicom_uid_arg) != dbImage.GetFieldAsString("DICOM_UID"));
+
+  // Error message if not found
+  if(!bReadOk)
     {
-    std::cerr << "Couldn't open first record of " << args_info.image_db_arg << std::endl;
-    exit(EXIT_FAILURE);
-    }
-  std::string currentDicomUidImage;
-  do
-    currentDicomUidImage = imageTable.GetField(NO_FIELD_DICOM_UID_IN_IMAGE);
-  while(currentDicomUidImage!=std::string(args_info.dicom_uid_arg) && imageTable.GetNextRecord() == XB_NO_ERROR);
-  if(currentDicomUidImage!=std::string(args_info.dicom_uid_arg))
-    {
-    std::cerr << "Couldn't file acquisition with DICOM_UID " << args_info.dicom_uid_arg
+    std::cerr << "Couldn't find acquisition with DICOM_UID " << args_info.dicom_uid_arg
               << " in table " << args_info.image_db_arg << std::endl;
     exit(EXIT_FAILURE);
     }
-    currentDicomUidImage = imageTable.GetField(NO_FIELD_DICOM_UID_IN_IMAGE);
 
-
-  const std::string imageID(imageTable.GetField(NO_FIELD_ID_IMAGE_IN_IMAGE));
-  imageTable.CloseDatabase();
-  return imageID;
+  return dbImage.GetFieldAsString("DBID");
 }
 
 void GetProjInfoFromDB(const std::string &imageID,
@@ -56,32 +38,23 @@ void GetProjInfoFromDB(const std::string &imageID,
                        std::vector<float> &projFlexX,
                        std::vector<float> &projFlexY)
 {
-  //Open image database
-  xbXBase db;
-
-  xbShort rc;
-  xbDbf frameTable(&db);
-  rc = frameTable.OpenDatabase( args_info.frame_db_arg );
-  if(rc!=XB_NO_ERROR){
+  // Open frame database file
+  rtk::DbfFile dbFrame(args_info.frame_db_arg);
+  if (!dbFrame.is_open()){
     std::cerr << "Couldn't open " << args_info.frame_db_arg << std::endl;
     exit(EXIT_FAILURE);
   }
-  rc = frameTable.GetFirstRecord();
-  if(rc!=XB_NO_ERROR)
-    {
-    std::cerr << "Couldn't open first record of " << args_info.frame_db_arg << std::endl;
-    exit(EXIT_FAILURE);
-    }
 
-  do
-    if(frameTable.GetField(NO_FIELD_ID_IMAGE_IN_FRAME) == imageID)
+  // Go through the database, select correct records and get data
+  while( dbFrame.ReadNextRecord() )
+    {
+    if(dbFrame.GetFieldAsString("IMA_DBID") == imageID)
       {
-      projAngle.push_back(frameTable.GetFloatField(NO_FIELD_ANGLE_IN_FRAME));
-      projFlexX.push_back(frameTable.GetFloatField(NO_FIELD_X_FLEX_IN_FRAME));
-      projFlexY.push_back(frameTable.GetFloatField(NO_FIELD_Y_FLEX_IN_FRAME));
+      projAngle.push_back(dbFrame.GetFieldAsDouble("PROJ_ANG"));
+      projFlexX.push_back(dbFrame.GetFieldAsDouble("U_CENTRE"));
+      projFlexY.push_back(dbFrame.GetFieldAsDouble("V_CENTRE"));
       }
-  while(frameTable.GetNextRecord() == XB_NO_ERROR);
-  frameTable.CloseDatabase();
+    }
 }
 
 int main(int argc, char * argv[])
