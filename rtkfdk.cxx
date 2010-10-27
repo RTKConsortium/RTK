713f20df47f0b286f888264a877cc6e7080bd359
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2010-10-27 19:30:37 +0200

    Default computation to float

--- rtkfdk.cxx
+++ rtkfdk.cxx
@@ -16,7 +16,7 @@ int main(int argc, char * argv[])
 {
   GGO(rtkfdk, args_info);
 
-  typedef double OutputPixelType;
+  typedef float OutputPixelType;
   const unsigned int Dimension = 3;
 
   typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
