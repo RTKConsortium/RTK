70e30c04ce48107acd5e4e3dfb3abac369640228
Greg Sharp <gregsharp@geocities.com>
2010-11-06 20:41:55 -0400

    Remove test file; copy FindOpenCL.cmake from plastimatch

---
+++ cmake/FindOpenCL.cmake
@@ -0,0 +1,78 @@
+##-----------------------------------------------------------------------------
+##  As posted on NVidia forum
+##  http://forums.nvidia.com/index.php?showtopic=97795
+##  Version: Oct 5, 2009
+##  Downloaded: Nov 14, 2009
+##  Modified by GCS
+##-----------------------------------------------------------------------------
+
+## AMD/ATI
+set(ENV_ATISTREAMSDKROOT $ENV{ATISTREAMSDKROOT})
+
+if(ENV_ATISTREAMSDKROOT)
+  find_path(
+    OPENCL_INCLUDE_DIR
+    NAMES CL/cl.h OpenCL/cl.h
+    PATHS $ENV{ATISTREAMSDKROOT}/include
+    NO_DEFAULT_PATH
+    )
+
+  ## Both windows and linux follow this directory structure.  Not sure 
+  ## about darwin.
+  if(CMAKE_SIZEOF_VOID_P EQUAL 4)
+    set(
+      OPENCL_LIB_SEARCH_PATH
+      ${OPENCL_LIB_SEARCH_PATH}
+      $ENV{ATISTREAMSDKROOT}/lib/x86
+      )
+  else(CMAKE_SIZEOF_VOID_P EQUAL 4)
+    set(
+      OPENCL_LIB_SEARCH_PATH
+      ${OPENCL_LIB_SEARCH_PATH}
+      $ENV{ATISTREAMSDKROOT}/lib/x86_64
+      )
+  endif(CMAKE_SIZEOF_VOID_P EQUAL 4)
+
+  find_library(
+    OPENCL_LIBRARY
+    NAMES OpenCL
+    PATHS ${OPENCL_LIB_SEARCH_PATH}
+    NO_DEFAULT_PATH
+    )
+
+## NVIDIA
+else(ENV_ATISTREAMSDKROOT)
+  find_path(
+    OPENCL_INCLUDE_DIR
+    PATHS $ENV{CUDA_INC_PATH}
+    NAMES CL/cl.h OpenCL/cl.h
+    )
+
+  find_library(
+    OPENCL_LIBRARY
+    PATHS $ENV{CUDA_LIB_PATH}
+    NAMES OpenCL
+    )
+endif(ENV_ATISTREAMSDKROOT)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+  OPENCL
+  DEFAULT_MSG
+  OPENCL_LIBRARY OPENCL_INCLUDE_DIR
+  )
+
+if(OPENCL_FOUND)
+  set(OPENCL_LIBRARIES ${OPENCL_LIBRARY})
+else(OPENCL_FOUND)
+  set(OPENCL_LIBRARIES)
+endif(OPENCL_FOUND)
+
+if(MINGW)
+  set(OPENCL_FOUND FALSE)
+endif(MINGW)
+
+mark_as_advanced(
+  OPENCL_INCLUDE_DIR
+  OPENCL_LIBRARY
+)
