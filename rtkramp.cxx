5dbe583800b60f949f2aed3e0786806a2c22123a
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2010-11-19 09:30:31 +0100

    Timing and streaming

--- rtkramp.cxx
+++ rtkramp.cxx
@@ -6,8 +6,8 @@
 
 #include <itkImageFileWriter.h>
 #include <itkRegularExpressionSeriesFileNames.h>
+#include <itkStreamingImageFilter.h>
 #include <itkTimeProbe.h>
-
 
 int main(int argc, char * argv[])
 {
@@ -30,19 +30,47 @@ int main(int argc, char * argv[])
   ReaderType::Pointer reader = ReaderType::New();
   reader->SetFileNames( names->GetFileNames() );
 
+  try {
+    reader->Update();
+  } catch( itk::ExceptionObject & err ) {
+    std::cerr << "ExceptionObject caught !" << std::endl;
+    std::cerr << err << std::endl;
+    return EXIT_FAILURE;
+  }
+
   // Ramp filter
   typedef itk::FFTRampImageFilter<OutputImageType> rampFilterType;
   rampFilterType::Pointer rampFilter = rampFilterType::New();
   rampFilter->SetInput( reader->GetOutput() );
   rampFilter->SetTruncationCorrection(args_info.pad_arg);
 
+  // Streaming filter
+  typedef itk::StreamingImageFilter<OutputImageType, OutputImageType> StreamerType;
+  StreamerType::Pointer streamer = StreamerType::New();
+  streamer->SetInput( rampFilter->GetOutput() );
+  streamer->SetNumberOfStreamDivisions( 1 + reader->GetOutput()->GetLargestPossibleRegion().GetNumberOfPixels() / (1024*1024*4) );
+
+  itk::TimeProbe probe;
+  try {
+    probe.Start();
+    streamer->Update();
+    probe.Stop();
+    std::cout << "The streamed ramp filter update took "
+              << probe.GetMeanTime()
+              << probe.GetUnit()
+              << std::endl;
+  } catch( itk::ExceptionObject & err ) {
+    std::cerr << "ExceptionObject caught !" << std::endl;
+    std::cerr << err << std::endl;
+    return EXIT_FAILURE;
+  }
+
   // Write
   typedef itk::ImageFileWriter<  OutputImageType >  WriterType;
   WriterType::Pointer writer = WriterType::New();
   writer->SetFileName( args_info.output_arg );
-  writer->SetInput( rampFilter->GetOutput() );
+  writer->SetInput( streamer->GetOutput() );
   writer->UpdateOutputInformation();
-  writer->SetNumberOfStreamDivisions( 1 + rampFilter->GetOutput()->GetLargestPossibleRegion().GetNumberOfPixels() / (1024*1024*4) );
 
   try {
     writer->Update();
