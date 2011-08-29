dbd6595a454cc8b8d81ff689ffd4bf48626c8bee
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2011-08-29 10:12:00 +0200

    Removed plastimatch specific code

--- cmake/FindCUDA_wrap.cmake
+++ cmake/FindCUDA_wrap.cmake
@@ -32,22 +32,12 @@ endif ()
 #   This script will modify CUDA_NVCC_FLAGS if system default is not gcc-4.3
 include (nvcc-check)
 
-# JAS 2010.12.09
-#   Build code for all known compute capabilities by default.
-#   When developing, it is sometimes nice to turn this off in order
-#   to speed up the build processes (since you only have 1 GPU in your machine).
-set (PLM_CUDA_ALL_DEVICES ON CACHE BOOL 
-  "Generate GPU code for all compute capabilities?")
-if (PLM_CUDA_ALL_DEVICES)
-  message (STATUS "CUDA Build Level: ALL Compute Capabilities")
-  set (CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS}
-        -gencode arch=compute_10,code=sm_10
-        -gencode arch=compute_11,code=sm_11
-        -gencode arch=compute_12,code=sm_12
-        -gencode arch=compute_13,code=sm_13
-        -gencode arch=compute_20,code=sm_20
+message (STATUS "CUDA Build Level: ALL Compute Capabilities")
+set (CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS}
+      -gencode arch=compute_10,code=sm_10
+      -gencode arch=compute_11,code=sm_11
+      -gencode arch=compute_12,code=sm_12
+      -gencode arch=compute_13,code=sm_13
+      -gencode arch=compute_20,code=sm_20
     )
-else ()
-  message (STATUS "CUDA Build Level: Build system Compute Capability ONLY!")
-endif ()
 
