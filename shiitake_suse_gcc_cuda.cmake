dd5340e3d74c2f2f5012bdeb0c34a87a5acdf1bf
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2011-10-12 19:02:43 +0200

    Initial cuda and opencl ctest script files

---
+++ shiitake_suse_gcc_cuda.cmake
@@ -0,0 +1,11 @@
+# client maintainer: me@mydomain.net
+set(CTEST_SITE "shiitake.clb")
+set(CTEST_BUILD_NAME "Linux-64bit-gcc-cuda")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(dashboard_binary_name "RTK_lin64_gcc_cuda")
+set(ENV{ITK_DIR} "/home/srit/src/itk/lin64")
+set(ENV{CUDA_BIN_PATH} "/home/srit/Download/cuda/cuda/bin")
+
+include("${CTEST_SCRIPT_DIRECTORY}/rtk_common.cmake")
+
---
+++ shiitake_suse_gcc_opencl.cmake
@@ -0,0 +1,11 @@
+# client maintainer: me@mydomain.net
+set(CTEST_SITE "shiitake.clb")
+set(CTEST_BUILD_NAME "Linux-64bit-gcc-opencl")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(dashboard_binary_name "RTK_lin64_gcc_opencl")
+set(ENV{ITK_DIR} "/home/srit/src/itk/lin64")
+set(ENV{LD_LIBRARY_PATH} "/home/srit/Download/AMD-APP-SDK-v2.5-lnx64/lib/x86_64:$ENV{LD_LIBRARY_PATH}")
+
+include("${CTEST_SCRIPT_DIRECTORY}/rtk_common.cmake")
+
