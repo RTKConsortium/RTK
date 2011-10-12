ffcbbde8fed1d05c9a523398f4e9fd3eada129da
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2011-10-12 19:09:16 +0200

    Script to run all ctests script sequentially on shiitake

---
+++ shiitake_lin64.sh
@@ -0,0 +1,4 @@
+ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_cuda.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_opencl.cmake -V
+
