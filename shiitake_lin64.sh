473d2fd9521b0b2a8b6ff975baae8bff22c31470
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2011-10-13 09:53:17 +0200

    Added intel compiler

--- shiitake_lin64.sh
+++ shiitake_lin64.sh
@@ -1,4 +1,5 @@
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_cuda.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_opencl.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_intel.cmake -V
 
