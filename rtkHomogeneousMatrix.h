f5154a9f094df8921c33052b6825619ab2e5f69e
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2011-10-24 17:28:57 +0200

    Moved common geometry functions to the 3D geometry object

--- rtkHomogeneousMatrix.h
+++ rtkHomogeneousMatrix.h
@@ -5,41 +5,6 @@
 #include <itkImage.h>
 
 //--------------------------------------------------------------------
-itk::Matrix< double, 3, 3 >
-Get2DScalingHomogeneousMatrix( double scalingX, double scalingY );
-
-//--------------------------------------------------------------------
-itk::Matrix< double, 3, 3 >
-Get2DRigidTransformationHomogeneousMatrix( double angleX, double transX, double transY );
-
-//--------------------------------------------------------------------
-itk::Matrix< double, 4, 4 >
-Get3DTranslationHomogeneousMatrix( double transX, double transY, double transZ );
-
-//--------------------------------------------------------------------
-itk::Matrix< double, 4, 4 >
-Get3DRigidTransformationHomogeneousMatrix( double angleX, double angleY, double angleZ,
-                                           double transX, double transY, double transZ );
-
-//--------------------------------------------------------------------
-itk::Matrix< double, 4, 4 >
-Get3DRotationHomogeneousMatrix( itk::Vector<double, 3> axis, double angle );
-
-//--------------------------------------------------------------------
-template< unsigned int TDimension >
-itk::Matrix< double, TDimension, TDimension+1 >
-GetProjectionMagnificationMatrix( double sdd, double sid )
-{
-  itk::Matrix< double, TDimension, TDimension+1 > matrix;
-  matrix.Fill(0.0);
-  for(unsigned int i=0; i<TDimension-1; i++)
-    matrix[i][i] = sdd;
-  matrix[TDimension-1][TDimension-1] = 1.0;
-  matrix[TDimension-1][TDimension  ] = sid;
-  return matrix;
-}
-
-//--------------------------------------------------------------------
 /** Get IndexToPhysicalPoint matrix from an image (no accessor provided by ITK) */
 //template <class TPixel, unsigned int VImageDimension>
 template <class TImageType>
