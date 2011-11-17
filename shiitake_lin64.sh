f306917ce8ea96d4c0cde1e0d1d9c49bdea12db8
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2011-11-17 13:21:23 +0100

    Doing both cuda 4.0 and cuda 3.2 compilation

--- shiitake_lin64.sh
+++ shiitake_lin64.sh
@@ -1,5 +1,6 @@
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc.cmake -V
-ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_cuda.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_cuda40.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_cuda32.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_opencl.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_intel.cmake -V
 
---
+++ shiitake_suse_gcc_cuda32.cmake
@@ -0,0 +1,11 @@
+# client maintainer: simon.rit@creatis.insa-lyon.fr
+set(CTEST_SITE "shiitake.clb")
+set(CTEST_BUILD_NAME "Linux-64bit-gcc-cuda32")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(dashboard_binary_name "RTK_lin64_gcc_cuda32")
+set(ENV{ITK_DIR} "/home/srit/src/itk/lin64")
+set(ENV{CUDA_BIN_PATH} "/home/srit/Download/cuda32/bin")
+
+include("${CTEST_SCRIPT_DIRECTORY}/rtk_common.cmake")
+
