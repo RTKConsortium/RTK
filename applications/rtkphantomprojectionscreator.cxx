789b6706895c1d1155247f9264b82337a15d5968
Marc Vila <marc.vilaoliva@uclouvain.be>
2012-05-11 10:55:21 +0200

    New application for creating any kind of phantom shape from a phantom config file.

---
+++ applications/rtkphantomprojectionscreator.cxx
@@ -0,0 +1,79 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "rtkphantomprojectionscreator_ggo.h"
+#include "rtkGgoFunctions.h"
+
+#include "rtkThreeDCircularProjectionGeometryXMLFile.h"
+#include "rtkRayEllipsoidIntersectionImageFilter.h"
+#include "rtkPhantomProjectionsCreatorFilter.h"
+
+#include <itkImageFileReader.h>
+#include <itkImageFileWriter.h>
+
+
+int main(int argc, char * argv[])
+{
+  GGO(rtkphantomprojectionscreator, args_info);
+
+  typedef float OutputPixelType;
+  const unsigned int Dimension = 3;
+
+  typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
+
+  // Geometry
+  if(args_info.verbose_flag)
+    std::cout << "Reading geometry information from "
+              << args_info.geometry_arg
+              << "..."
+              << std::endl;
+
+  rtk::ThreeDCircularProjectionGeometryXMLFileReader::Pointer geometryReader;
+  geometryReader = rtk::ThreeDCircularProjectionGeometryXMLFileReader::New();
+  geometryReader->SetFilename(args_info.geometry_arg);
+  TRY_AND_EXIT_ON_ITK_EXCEPTION( geometryReader->GenerateOutputInformation() )
+
+  // Create a stack of empty projection images
+  typedef rtk::ConstantImageSource< OutputImageType > ConstantImageSourceType;
+  ConstantImageSourceType::Pointer constantImageSource = ConstantImageSourceType::New();
+  rtk::SetConstantImageSourceFromGgo<ConstantImageSourceType, args_info_rtkphantomprojectionscreator>(constantImageSource, args_info);
+
+  // Adjust size according to geometry
+  ConstantImageSourceType::SizeType sizeOutput;
+  sizeOutput[0] = constantImageSource->GetSize()[0];
+  sizeOutput[1] = constantImageSource->GetSize()[1];
+  sizeOutput[2] = geometryReader->GetOutputObject()->GetGantryAngles().size();
+  constantImageSource->SetSize( sizeOutput );
+
+  typedef rtk::PhantomProjectionsCreatorFilter<OutputImageType, OutputImageType> PPCType;
+  PPCType::Pointer ppc = PPCType::New();
+  ppc->SetInput(constantImageSource->GetOutput());
+  ppc->SetGeometry(geometryReader->GetOutputObject());
+  ppc->SetConfigFile(args_info.phantomfile_arg);
+  ppc->Update();
+  // Write
+  typedef itk::ImageFileWriter<  OutputImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( args_info.output_arg );
+  writer->SetInput( ppc->GetOutput() );
+  if(args_info.verbose_flag)
+    std::cout << "Projecting and writing... " << std::flush;
+  TRY_AND_EXIT_ON_ITK_EXCEPTION( writer->Update() );
+
+  return EXIT_SUCCESS;
+}
