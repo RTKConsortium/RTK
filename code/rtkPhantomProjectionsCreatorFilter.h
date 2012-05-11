6eae16e0c8b9067f227fb15a6aa55a0a58f8f6eb
Marc Vila <marc.vilaoliva@uclouvain.be>
2012-05-11 10:57:52 +0200

    New filter that creates any kind of phantom, passed by a config file.

---
+++ code/rtkPhantomProjectionsCreatorFilter.h
@@ -0,0 +1,99 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef __rtkPhantomProjectionsCreatorFilter_h
+#define __rtkPhantomProjectionsCreatorFilter_h
+
+#include <itkInPlaceImageFilter.h>
+#include "rtkThreeDCircularProjectionGeometry.h"
+#include "rtkRayQuadricIntersectionImageFilter.h"
+#include "rtkConfigFileReader.h"
+
+#include "rtkThreeDCircularProjectionGeometryXMLFile.h"
+#include "rtkRayEllipsoidIntersectionImageFilter.h"
+
+#include <vector>
+
+namespace rtk
+{
+
+/** \class PhantomProjectionsCreatorFilter
+ * \brief Computes intersection of projection rays with ellipsoids.
+ * in order to create the projections of a specific phantom.
+ */
+
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT PhantomProjectionsCreatorFilter :
+  public RayEllipsoidIntersectionImageFilter<TInputImage,TOutputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef PhantomProjectionsCreatorFilter                                       Self;
+  typedef RayEllipsoidIntersectionImageFilter<TInputImage,TOutputImage> Superclass;
+  typedef itk::SmartPointer<Self>                                       Pointer;
+  typedef itk::SmartPointer<const Self>                                 ConstPointer;
+  typedef typename TOutputImage::RegionType               OutputImageRegionType;
+  typedef typename TOutputImage::Superclass::ConstPointer OutputImageBaseConstPointer;
+
+  typedef float OutputPixelType;
+
+  typedef itk::Image< OutputPixelType, 3 >                                           OutputImageType;
+  typedef rtk::RayEllipsoidIntersectionImageFilter<OutputImageType, OutputImageType> REIType;
+  typedef std::vector<double>                                                        VectorType;
+  typedef std::string                                                                StringType;
+  typedef std::vector< std::vector<double> >                                         VectorOfVectorType;
+  typedef rtk::ConfigFileReader                                                      CFRType;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(PhantomProjectionsCreatorFilter, RayEllipsoidIntersectionImageFilter);
+
+  /** Get/Set Number of Figures.*/
+  itkSetMacro(ConfigFile, StringType);
+  itkGetMacro(ConfigFile, StringType);
+
+  rtkSetMacro(Fig, VectorOfVectorType);
+  rtkGetMacro(Fig, VectorOfVectorType);
+
+protected:
+  PhantomProjectionsCreatorFilter() {}
+  virtual ~PhantomProjectionsCreatorFilter() {};
+
+  virtual void GenerateData();
+
+  /** Translate user parameteres to quadric parameters.
+   * A call to this function will assume modification of the function.*/
+
+
+private:
+  PhantomProjectionsCreatorFilter(const Self&); //purposely not implemented
+  void operator=(const Self&);            //purposely not implemented
+
+  VectorOfVectorType       m_Fig;
+  StringType               m_ConfigFile;
+};
+
+} // end namespace rtk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "rtkPhantomProjectionsCreatorFilter.txx"
+#endif
+
+#endif
---
+++ code/rtkPhantomProjectionsCreatorFilter.txx
@@ -0,0 +1,87 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef __rtkPhantomProjectionsCreatorFilter_txx
+#define __rtkPhantomProjectionsCreatorFilter_txx
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <itkImageRegionConstIterator.h>
+#include <itkImageRegionIteratorWithIndex.h>
+#include "rtkHomogeneousMatrix.h"
+
+namespace rtk
+{
+template< class TInputImage, class TOutputImage >
+void PhantomProjectionsCreatorFilter< TInputImage, TOutputImage >::GenerateData()
+{
+  CFRType::Pointer cfr = CFRType::New();
+  cfr->Config(m_ConfigFile);
+  m_Fig = cfr->GetFig();
+
+  std::vector< REIType::Pointer > rei( m_Fig.size() );
+  for ( unsigned int i = 0; i < m_Fig.size(); i++ )
+    {
+    rei[i] = REIType::New();
+    //Set GrayScale value, axes, center...
+    rei[i]->SetMultiplicativeConstant(m_Fig[i][7]);
+    rei[i]->SetSemiPrincipalAxisX(m_Fig[i][0]);
+    rei[i]->SetSemiPrincipalAxisY(m_Fig[i][1]);
+    rei[i]->SetSemiPrincipalAxisZ(m_Fig[i][2]);
+
+    rei[i]->SetCenterX(m_Fig[i][3]);
+    rei[i]->SetCenterY(m_Fig[i][4]);
+    rei[i]->SetCenterZ(m_Fig[i][5]);
+
+    rei[i]->SetRotationAngle(m_Fig[i][6]);
+
+    if ( i == ( m_Fig.size() - 1 ) ) //last case
+      {
+      if(i==0) //just one ellipsoid
+        rei[i]->SetInput( rei[i]->GetOutput() );
+      else
+        rei[i]->SetInput( rei[i-1]->GetOutput() );
+      rei[i]->SetGeometry( this->GetGeometry() );
+      }
+
+    if (i>0) //other cases
+      {
+      rei[i]->SetInput( rei[i-1]->GetOutput() );
+      rei[i]->SetGeometry( this->GetGeometry() );
+      }
+
+    else //first case
+      {
+      rei[i]->SetInput( this->GetInput() );
+      rei[i]->SetGeometry( this->GetGeometry() );
+      }
+    }
+  //Update
+  rei[ m_Fig.size() - 1]->Update();
+  this->GraftOutput( rei[m_Fig.size()-1]->GetOutput() );
+}
+} // end namespace rtk
+
+#endif
