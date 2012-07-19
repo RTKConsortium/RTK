7a55593fdbbd08b40141538537c94934d4a7af47
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2012-07-19 18:55:35 +0200

    Short scan test with smaller resolutions

--- testing/rtkshortscantest.cxx
+++ testing/rtkshortscantest.cxx
@@ -1,116 +1,39 @@
-#include <itkImageFileWriter.h>
-#include <itkImageFileReader.h>
 #include <itkImageRegionConstIterator.h>
-#include <itkRegularExpressionSeriesFileNames.h>
-#include <vcl_cmath.h>
 
 #include "rtkSheppLoganPhantomFilter.h"
-#include "rtkDrawQuadricImageFilter.h"
 #include "rtkDrawSheppLoganFilter.h"
 #include "rtkFDKConeBeamReconstructionFilter.h"
 #include "rtkConstantImageSource.h"
 #include "rtkParkerShortScanImageFilter.h"
 
-int main(int argc, char* argv[])
+template<class TImage>
+void CheckImageQuality(typename TImage::Pointer recon, typename TImage::Pointer ref)
 {
-  const unsigned int Dimension = 3;
-  typedef float                                    OutputPixelType;
-  typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
-
-  // RTK geometry object
-  typedef rtk::ThreeDCircularProjectionGeometry GeometryType;
-  GeometryType::Pointer geometry = GeometryType::New();
-  for(int noProj=0; noProj<210; noProj++)
-    {
-    geometry->AddProjection(1000.,
-                            1536.,
-                            noProj,
-                            0.,
-                            0.);
-    }
-  geometry->Update();
-
-  // Constant image sources
-  typedef rtk::ConstantImageSource< OutputImageType > ConstantImageSourceType;
-  ConstantImageSourceType::Pointer tomographySource  = ConstantImageSourceType::New();
-  ConstantImageSourceType::Pointer projectionsSource = ConstantImageSourceType::New();
-  ConstantImageSourceType::PointType origin;
-  origin.Fill(-179.5);
-  ConstantImageSourceType::SizeType size;
-  size.Fill(360);
-  ConstantImageSourceType::SpacingType spacing;
-  spacing.Fill(1.);
-  tomographySource->SetOrigin( origin );
-  tomographySource->SetSpacing( spacing );
-  tomographySource->SetSize( size );
-  tomographySource->SetConstant( 0. );
-  tomographySource->UpdateOutputInformation();
-  size[2]=210;
-  projectionsSource->SetOrigin( origin );
-  projectionsSource->SetSpacing( spacing );
-  projectionsSource->SetSize( size );
-  projectionsSource->SetConstant( 0. );
-  projectionsSource->UpdateOutputInformation();
-
-  typedef rtk::SheppLoganPhantomFilter<OutputImageType, OutputImageType> SLPType;
-  SLPType::Pointer slp=SLPType::New();
-  slp->SetInput( projectionsSource->GetOutput() );
-  slp->SetGeometry( geometry );
-  slp->Update();
-
-  // Short scan image filter
-  typedef rtk::ParkerShortScanImageFilter< OutputImageType > PSSFType;
-  PSSFType::Pointer pssf = PSSFType::New();
-  pssf->SetInput( slp->GetOutput() );
-  pssf->SetGeometry( geometry );
-  pssf->InPlaceOff();
-
-  // FDK reconstruction filtering
-  typedef rtk::FDKConeBeamReconstructionFilter< OutputImageType > FDKCPUType;
-  FDKCPUType::Pointer feldkamp = FDKCPUType::New();
-  feldkamp->SetInput( 0, tomographySource->GetOutput() );
-  feldkamp->SetInput( 1, pssf->GetOutput() );
-  feldkamp->SetGeometry( geometry );
-  feldkamp->GetRampFilter()->SetTruncationCorrection(0.);
-  feldkamp->GetRampFilter()->SetHannCutFrequency(0.);
-  TRY_AND_EXIT_ON_ITK_EXCEPTION( feldkamp->Update() )
-
-  // Create a reference object (in this case a 3D phantom reference).
-  typedef rtk::DrawSheppLoganFilter<OutputImageType, OutputImageType> DSLType;
-  DSLType::Pointer dsl = DSLType::New();
-  dsl->SetInput( tomographySource->GetOutput() );
-  TRY_AND_EXIT_ON_ITK_EXCEPTION( dsl->Update() )
-
-  typedef itk::ImageRegionConstIterator<OutputImageType> ImageIteratorType;
-  ImageIteratorType itTest( feldkamp->GetOutput(), feldkamp->GetOutput()->GetBufferedRegion() );
-  ImageIteratorType itRef( dsl->GetOutput(), dsl->GetOutput()->GetBufferedRegion() );
+  typedef itk::ImageRegionConstIterator<TImage> ImageIteratorType;
+  ImageIteratorType itTest( recon, recon->GetBufferedRegion() );
+  ImageIteratorType itRef( ref, ref->GetBufferedRegion() );
 
   typedef double ErrorType;
-  ErrorType TestError     = 0.;
-  ErrorType TestTolerance = 0.005;
-  ErrorType EnerError     = 0.;
-  bool Exit               = true;
+  ErrorType TestError = 0.;
+  ErrorType EnerError = 0.;
 
   itTest.GoToBegin();
   itRef.GoToBegin();
 
   while( !itRef.IsAtEnd() )
     {
-    OutputPixelType TestVal = itTest.Get();
-    OutputPixelType RefVal = itRef.Get();
-    if( TestVal != RefVal )
-      {
-        TestError += abs(RefVal - TestVal);
-        EnerError += vcl_pow(ErrorType(RefVal - TestVal), 2.);
-      }
+    typename TImage::PixelType TestVal = itTest.Get();
+    typename TImage::PixelType RefVal = itRef.Get();
+    TestError += vcl_abs(RefVal - TestVal);
+    EnerError += vcl_pow(ErrorType(RefVal - TestVal), 2.);
     ++itTest;
     ++itRef;
     }
   // Error per Pixel
-  ErrorType ErrorPerPixel = TestError/(size[0]*size[1]*size[2]);
+  ErrorType ErrorPerPixel = TestError/ref->GetBufferedRegion().GetNumberOfPixels();
   std::cout << "\nError per Pixel = " << ErrorPerPixel << std::endl;
   // MSE
-  ErrorType MSE = EnerError/(size[0]*size[1]*size[2]);
+  ErrorType MSE = EnerError/ref->GetBufferedRegion().GetNumberOfPixels();
   std::cout << "MSE = " << MSE << std::endl;
   // PSNR
   ErrorType PSNR = 20*log10(2.0) - 10*log10(MSE);
@@ -118,37 +41,103 @@ int main(int argc, char* argv[])
   // QI
   ErrorType QI = (2.0-ErrorPerPixel)/2.0;
   std::cout << "QI = " << QI << std::endl;
+
   // Checking results
-  if (ErrorPerPixel > TestTolerance)
+  if (ErrorPerPixel > 0.09)
   {
-    std::cerr << "Test Failed, Error per pixel not valid!" << std::endl;
-    Exit = false;
+    std::cerr << "Test Failed, Error per pixel not valid! "
+              << ErrorPerPixel << " instead of 0.09." << std::endl;
+    exit( EXIT_FAILURE);
   }
-  if (PSNR < 23.0)
+  if (PSNR < 22)
   {
-    std::cerr << "Test Failed, PSNR not valid!" << std::endl;
-    Exit = false;
+    std::cerr << "Test Failed, PSNR not valid! "
+              << PSNR << " instead of 22" << std::endl;
+    exit( EXIT_FAILURE);
   }
+}
 
-  if(Exit)
-  {
-    std::cerr << "Test PASSED! " << std::endl;
-    return EXIT_SUCCESS;
-  }
-  else
-  {
-    // Write out the reconstructed image
-    typedef itk::ImageFileWriter<  OutputImageType > WriterType;
-    WriterType::Pointer writer = WriterType::New();
-    writer->SetFileName( "reconstruction.mha" );
-    writer->SetInput( feldkamp->GetOutput() );
-    TRY_AND_EXIT_ON_ITK_EXCEPTION( writer->Update() )
 
-    // Write out the reference image
-    writer->SetInput(dsl->GetOutput());
-    writer->SetFileName("reference.mha");
-    TRY_AND_EXIT_ON_ITK_EXCEPTION( writer->Update() )
+int main(int argc, char* argv[])
+{
+  const unsigned int Dimension = 3;
+  typedef float                                    OutputPixelType;
+  typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
+  const unsigned int NumberOfProjectionImages = 110;
+  const double ArcSize = 240.;
 
-    return EXIT_FAILURE;
-  }
+  // Constant image sources
+  typedef rtk::ConstantImageSource< OutputImageType > ConstantImageSourceType;
+  ConstantImageSourceType::PointType origin;
+  ConstantImageSourceType::SizeType size;
+  ConstantImageSourceType::SpacingType spacing;
+
+  ConstantImageSourceType::Pointer tomographySource  = ConstantImageSourceType::New();
+  origin[0] = -127.;
+  origin[1] = -127.;
+  origin[2] = -127.;
+  size[0] = 128;
+  size[1] = 128;
+  size[2] = 128;
+  spacing[0] = 2.;
+  spacing[1] = 2.;
+  spacing[2] = 2.;
+  tomographySource->SetOrigin( origin );
+  tomographySource->SetSpacing( spacing );
+  tomographySource->SetSize( size );
+  tomographySource->SetConstant( 0. );
+
+  ConstantImageSourceType::Pointer projectionsSource = ConstantImageSourceType::New();
+  origin[0] = -254.;
+  origin[1] = -254.;
+  origin[2] = -254.;
+  size[0] = 128;
+  size[1] = 128;
+  size[2] = NumberOfProjectionImages;
+  spacing[0] = 4.;
+  spacing[1] = 4.;
+  spacing[2] = 4.;
+  projectionsSource->SetOrigin( origin );
+  projectionsSource->SetSpacing( spacing );
+  projectionsSource->SetSize( size );
+  projectionsSource->SetConstant( 0. );
+
+  // Geometry object
+  typedef rtk::ThreeDCircularProjectionGeometry GeometryType;
+  GeometryType::Pointer geometry = GeometryType::New();
+  for(unsigned int noProj=0; noProj<NumberOfProjectionImages; noProj++)
+    geometry->AddProjection(600., 1200., noProj*ArcSize/NumberOfProjectionImages);
+
+  // Shepp Logan projections filter
+  typedef rtk::SheppLoganPhantomFilter<OutputImageType, OutputImageType> SLPType;
+  SLPType::Pointer slp=SLPType::New();
+  slp->SetInput( projectionsSource->GetOutput() );
+  slp->SetGeometry(geometry);
+  TRY_AND_EXIT_ON_ITK_EXCEPTION( slp->Update() );
+
+  // Short scan image filter
+  typedef rtk::ParkerShortScanImageFilter< OutputImageType > PSSFType;
+  PSSFType::Pointer pssf = PSSFType::New();
+  pssf->SetInput( slp->GetOutput() );
+  pssf->SetGeometry( geometry );
+  pssf->InPlaceOff();
+  pssf->Update();
+
+  // Create a reference object (in this case a 3D phantom reference).
+  typedef rtk::DrawSheppLoganFilter<OutputImageType, OutputImageType> DSLType;
+  DSLType::Pointer dsl = DSLType::New();
+  dsl->SetInput( tomographySource->GetOutput() );
+  TRY_AND_EXIT_ON_ITK_EXCEPTION( dsl->Update() )
+
+  // FDK reconstruction filtering
+  typedef rtk::FDKConeBeamReconstructionFilter< OutputImageType > FDKCPUType;
+  FDKCPUType::Pointer feldkamp = FDKCPUType::New();
+  feldkamp->SetInput( 0, tomographySource->GetOutput() );
+  feldkamp->SetInput( 1, pssf->GetOutput() );
+  feldkamp->SetGeometry( geometry );
+  TRY_AND_EXIT_ON_ITK_EXCEPTION( feldkamp->Update() );
+
+  CheckImageQuality<OutputImageType>(feldkamp->GetOutput(), dsl->GetOutput());
+  std::cout << "\n\nTest PASSED! " << std::endl;
+  exit(EXIT_SUCCESS);
 }
