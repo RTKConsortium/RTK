d9c9afcb3c8fb1b35e17c240e47e4278be8d9237
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2012-08-08 10:24:39 +0200

    Added doxygen and style builds.

--- shiitake_lin64.sh
+++ shiitake_lin64.sh
@@ -3,4 +3,8 @@ ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_cuda40.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_cuda32.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_opencl.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_intel.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_style.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_doxygen.cmake -V
+rsync -a --delete            /home/srit/src/rtk/dashboard_tests/RTK-Doxygen/Doxygen/html \
+    ssh.creatis.insa-lyon.fr:/home/srit/src/rtk/dashboard_tests/RTK-Doxygen/Doxygen
 
