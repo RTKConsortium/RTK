7adce9979e3aa40852e27940ebdcc403ad40c5f3
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2012-08-10 10:54:57 +0200

    Fixed sync issue

--- shiitake_lin64.sh
+++ shiitake_lin64.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -x
+
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_cuda40.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_cuda32.cmake -V
@@ -5,6 +7,7 @@ ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_opencl.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_intel.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_style.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_doxygen.cmake -V
-rsync -a --delete            /home/srit/src/rtk/dashboard_tests/RTK-Doxygen/Doxygen/html \
+rsync -e 'ssh -i /home/srit/.ssh/nophrase' -a --delete \
+    /home/srit/src/rtk/dashboard_tests/RTK-Doxygen/Doxygen/html \
     ssh.creatis.insa-lyon.fr:/home/srit/src/rtk/dashboard_tests/RTK-Doxygen/Doxygen
 
