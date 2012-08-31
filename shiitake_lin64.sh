1c9895e2cec33f066d2d553a521621344ea58aac
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2012-08-31 18:17:36 +0200

    Moved cuda tests to a cuda compatible machine.

--- shiitake_lin64.sh
+++ shiitake_lin64.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -x
 
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc.cmake -V
-ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_cuda40.cmake -V
-ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_cuda32.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_opencl.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_intel.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_style.cmake -V
