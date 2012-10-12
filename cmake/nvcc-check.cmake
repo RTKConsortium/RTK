1fb2d902f72ba3e5aa38f49e1ba648e917abccc7
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2012-10-12 11:13:29 +0200

    Some fixes taken from Plastimatch

--- cmake/nvcc-check.cmake
+++ cmake/nvcc-check.cmake
@@ -31,27 +31,47 @@ FUNCTION(FIND_GCC GCC_PATH GCC_MAJOR GCC_MINOR)
 
     # Check that adequate
     IF("${GCCVER_MAJOR}" MATCHES "${GCC_MAJOR}" AND "${GCCVER_MINOR}" MATCHES "${GCC_MINOR}")
-        SET(GCC_PATH ${DEFAULT_GCC} PARENT_SCOPE)
+      SET(GCC_PATH ${DEFAULT_GCC} PARENT_SCOPE)
     ENDIF()
   ENDIF(EXACT_GCC)
 
 ENDFUNCTION(FIND_GCC)
 
 # Main code dealing with each version of cuda
-IF(CUDA_FOUND AND CMAKE_SYSTEM_NAME MATCHES "Linux" AND CMAKE_COMPILER_IS_GNUCC)
-  SET(GCC_PATH "")
-  IF(CUDA_VERSION_MAJOR MATCHES "4")
-    FIND_GCC(GCC_PATH "4" "4")
-  ENDIF(CUDA_VERSION_MAJOR MATCHES "4")
-  IF(NOT GCC_PATH OR CUDA_VERSION_MAJOR LESS "3")
-    FIND_GCC(GCC_PATH "4" "3")
-  ENDIF(NOT GCC_PATH OR CUDA_VERSION_MAJOR LESS "3")
+IF(CUDA_FOUND)
+  IF(CMAKE_SYSTEM_NAME MATCHES "Linux" AND CMAKE_COMPILER_IS_GNUCC)
+    SET(GCC_PATH "")
+    IF(CUDA_VERSION_MAJOR MATCHES "4")
+      FIND_GCC(GCC_PATH "4" "4")
+    ENDIF(CUDA_VERSION_MAJOR MATCHES "4")
+    IF(NOT GCC_PATH OR CUDA_VERSION_MAJOR LESS "3")
+      FIND_GCC(GCC_PATH "4" "3")
+    ENDIF(NOT GCC_PATH OR CUDA_VERSION_MAJOR LESS "3")
 
-  IF(GCC_PATH)
-    MESSAGE(STATUS "nvcc-check: Found adequate gcc (${GCC_PATH})... telling nvcc to use it!")
-    SET(CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS} --compiler-bindir=${GCC_PATH})
-  ELSE(GCC_PATH)
-    MESSAGE(FATAL_ERROR "nvcc-check: Please install adequate gcc for cuda (gcc-4.3 or gcc-4.4 for Cuda 4).\nNote that gcc-4.x can be installed side-by-side with your current version of gcc.\n")
-  ENDIF(GCC_PATH)
-ENDIF(CUDA_FOUND AND CMAKE_SYSTEM_NAME MATCHES "Linux" AND CMAKE_COMPILER_IS_GNUCC)
+    IF(GCC_PATH)
+      MESSAGE(STATUS "nvcc-check: Found adequate gcc (${GCC_PATH})... telling nvcc to use it!")
+      SET(CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS} --compiler-bindir=${GCC_PATH})
+    ELSE(GCC_PATH)
+      MESSAGE(FATAL_ERROR "nvcc-check: Please install adequate gcc for cuda (gcc-4.3 or gcc-4.4 for Cuda 4).\nNote that gcc-4.x can be installed side-by-side with your current version of gcc.\n")
+    ENDIF(GCC_PATH)
+  ENDIF(CMAKE_SYSTEM_NAME MATCHES "Linux" AND CMAKE_COMPILER_IS_GNUCC)
 
+
+  IF(CMAKE_SYSTEM_NAME MATCHES "Linux" OR "APPLE")
+      # For CUDA 3.2: surface_functions.h does some non-compliant things...
+      #               so we tell g++ to ignore them when called via nvcc
+      #               by passing the -fpermissive flag through the nvcc
+      #               build trajectory.  Unfortunately, nvcc will also
+      #               blindly pass this flag to gcc, even though it is not
+      #               valid... resulting in TONS of warnings.  So, we go
+      #               version checking again, this time nvcc...
+      # Get the nvcc version number
+
+      IF(CUDA_VERSION_MAJOR MATCHES "3")
+          IF(CUDA_VERSION_MINOR MATCHES "2")
+              SET (CUDA_NVCC_FLAGS ${CUDA_NVCC_FLAGS} --compiler-options='-fpermissive')
+              MESSAGE(STATUS "nvcc-check: CUDA 3.2 exception: CUDA_NVCC_FLAGS set to \"${CUDA_NVCC_FLAGS}\"")
+          ENDIF()
+      ENDIF()
+  ENDIF()
+ENDIF(CUDA_FOUND)
