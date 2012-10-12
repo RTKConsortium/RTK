3a12b8c619fd695d428cda344b1128fb77b3881e
Marc Vila <marc.vilaoliva@uclouvain.be>
2012-10-12 18:59:46 +0200

    New CUDA forward projection filter based on Nifty version

---
+++ code/rtkCudaForwardProjectionImageFilter.cxx
@@ -0,0 +1,147 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "rtkCudaForwardProjectionImageFilter.h"
+#include "rtkCudaUtilities.hcu"
+#include "rtkCudaForwardProjectionImageFilter.hcu"
+
+#include <itkImageRegionConstIterator.h>
+#include <itkImageRegionIteratorWithIndex.h>
+#include <itkLinearInterpolateImageFunction.h>
+#include <itkMacro.h>
+
+namespace rtk
+{
+
+CudaForwardProjectionImageFilter
+::CudaForwardProjectionImageFilter()
+{
+  m_DeviceVolume     = NULL;
+  m_DeviceProjection = NULL;
+  m_DeviceMatrix     = NULL;
+}
+
+void
+CudaForwardProjectionImageFilter
+::InitDevice()
+{
+  // Dimension arguments in CUDA format for volume
+  m_VolumeDimension[0] = this->GetInput(1)->GetRequestedRegion().GetSize()[0];
+  m_VolumeDimension[1] = this->GetInput(1)->GetRequestedRegion().GetSize()[1];
+  m_VolumeDimension[2] = this->GetInput(1)->GetRequestedRegion().GetSize()[2];
+  // Dimension arguments in CUDA format for projections
+  m_ProjectionDimension[0] = this->GetInput(0)->GetRequestedRegion().GetSize()[0];
+  m_ProjectionDimension[1] = this->GetInput(0)->GetRequestedRegion().GetSize()[1];
+  // Cuda initialization
+  std::vector<int> devices = GetListOfCudaDevices();
+  if(devices.size()>1)
+    {
+    cudaThreadExit();
+    cudaSetDevice(devices[0]);
+    }
+  const float *host_volume = this->GetInput(1)->GetBufferPointer();
+  CUDA_forward_project_init (m_ProjectionDimension, m_VolumeDimension,
+                             m_DeviceVolume, m_DeviceProjection, m_DeviceMatrix, host_volume);
+}
+
+void
+CudaForwardProjectionImageFilter
+::CleanUpDevice()
+{
+  if(this->GetOutput()->GetRequestedRegion() != this->GetOutput()->GetBufferedRegion() )
+    itkExceptionMacro(<< "Can't handle different requested and buffered regions "
+                      << this->GetOutput()->GetRequestedRegion()
+                      << this->GetOutput()->GetBufferedRegion() );
+  CUDA_forward_project_cleanup (m_ProjectionDimension,
+                                m_DeviceVolume,
+                                m_DeviceProjection,
+                                m_DeviceMatrix);
+  m_DeviceVolume     = NULL;
+  m_DeviceProjection = NULL;
+  m_DeviceMatrix     = NULL;
+}
+
+void
+CudaForwardProjectionImageFilter
+::GenerateData()
+{
+  this->AllocateOutputs();
+  this->InitDevice();
+  const Superclass::GeometryType::Pointer geometry = this->GetGeometry();
+  const unsigned int Dimension = ImageType::ImageDimension;
+  const unsigned int iFirstProj = this->GetInput(0)->GetRequestedRegion().GetIndex(Dimension-1);
+  const unsigned int nProj = this->GetInput(0)->GetRequestedRegion().GetSize(Dimension-1);
+  const unsigned int nPixelsPerProj = this->GetOutput()->GetBufferedRegion().GetSize(0) *
+                                      this->GetOutput()->GetBufferedRegion().GetSize(1);
+  float t_step = 1; // Step in mm
+  int blockSize[3] = {16,16,1};
+  itk::Vector<double, 4> source_position;
+
+  // Setting BoxMin and BoxMax
+  float boxMin[3];
+  float boxMax[3];
+  for(unsigned int i=0; i<3; i++)
+    {
+      boxMin[i] = this->GetInput(1)->GetBufferedRegion().GetIndex()[i]-0.5;
+      boxMax[i] = boxMin[i] + this->GetInput(1)->GetBufferedRegion().GetSize()[i];
+    }
+
+  // Getting Spacing
+  float spacing[3];
+  for(unsigned int i=0; i<3; i++)
+    spacing[i] = this->GetInput(1)->GetSpacing()[i];
+
+  // Go over each projection
+  for(unsigned int iProj=iFirstProj; iProj<iFirstProj+nProj; iProj++)
+    {
+      // Account for system rotations
+      Superclass::GeometryType::ThreeDHomogeneousMatrixType volPPToIndex;
+      volPPToIndex = GetPhysicalPointToIndexMatrix( this->GetInput(1) );
+
+      // Adding an offset to locate the center of my voxels in CUDA
+      for(unsigned int i=0; i<3; i++)
+        volPPToIndex[i][3]+=0.5;
+
+      // Compute matrix to transform projection index to volume index
+      Superclass::GeometryType::ThreeDHomogeneousMatrixType d_matrix;
+      d_matrix = volPPToIndex.GetVnlMatrix() *
+                 geometry->GetProjectionCoordinatesToFixedSystemMatrix(iProj).GetVnlMatrix() *
+                 GetIndexToPhysicalPointMatrix( this->GetInput() ).GetVnlMatrix();
+      float matrix[4][4];
+      for (int j=0; j<4; j++)
+        for (int k=0; k<4; k++)
+          matrix[j][k] = (float)d_matrix[j][k];
+
+      // Set source position in volume indices
+      source_position = volPPToIndex * geometry->GetSourcePosition(iProj);
+      CUDA_forward_project(blockSize,
+                           this->GetOutput()->GetBufferPointer()+iProj*nPixelsPerProj,
+                           m_DeviceProjection,
+                           (double*)&(source_position[0]),
+                           m_ProjectionDimension,
+                           t_step,
+                           m_DeviceMatrix,
+                           (float*)&(matrix[0][0]),
+                           boxMin,
+                           boxMax,
+                           spacing);
+    }
+  this->CleanUpDevice();
+}
+
+} // end namespace rtk
---
+++ code/rtkCudaForwardProjectionImageFilter.h
@@ -0,0 +1,91 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef __rtkCudaForwardProjectionImageFilter_h
+#define __rtkCudaForwardProjectionImageFilter_h
+
+#include "rtkJosephForwardProjectionImageFilter.h"
+
+/** \class CudaForwardProjectionImageFilter
+ * \brief TODO
+ *
+ * TODO
+ *
+ * \author TODO 
+ *
+ * \ingroup Projector CudaImageToImageFilter
+ */
+
+namespace rtk
+{
+
+class ITK_EXPORT CudaForwardProjectionImageFilter :
+  public ForwardProjectionImageFilter< itk::Image<float,3>, itk::Image<float,3> >
+{
+public:
+  /** Standard class typedefs. */
+  typedef itk::Image<float,3>                                ImageType;
+  typedef CudaForwardProjectionImageFilter                   Self;
+  typedef ForwardProjectionImageFilter<ImageType, ImageType> Superclass;
+  typedef itk::SmartPointer<Self>                            Pointer;
+  typedef itk::SmartPointer<const Self>                      ConstPointer;
+
+  typedef ImageType::RegionType        OutputImageRegionType;
+  typedef itk::Image<float, 2>         ProjectionImageType;
+  typedef ProjectionImageType::Pointer ProjectionImagePointer;
+  typedef itk::Vector<float,3>         VectorType;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(CudaForwardProjectionImageFilter, ImageToImageFilter);
+
+  /** Function to allocate memory on device */
+  void InitDevice();
+
+  /** Function to synchronize memory from device to host and free device memory */
+  void CleanUpDevice();
+
+  /** Boolean to keep the hand on the memory management of the GPU. Default is
+   * off. If on, the user must call manually InitDevice and CleanUpDevice. */
+  itkGetMacro(ExplicitGPUMemoryManagementFlag, bool);
+  itkSetMacro(ExplicitGPUMemoryManagementFlag, bool);
+
+protected:
+  CudaForwardProjectionImageFilter();
+  ~CudaForwardProjectionImageFilter() {};
+
+  void GenerateData();
+
+private:
+  //purposely not implemented
+  CudaForwardProjectionImageFilter(const Self&);
+  void operator=(const Self&);
+
+  int                m_VolumeDimension[3];
+  int                m_ProjectionDimension[2];
+  float *            m_DeviceVolume;
+  float *            m_DeviceProjection;
+  float *            m_DeviceMatrix;
+  bool               m_ExplicitGPUMemoryManagementFlag;
+}; // end of class
+
+} // end namespace rtk
+
+#endif
