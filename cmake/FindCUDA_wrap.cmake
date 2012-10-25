ab49e2f29047ae2851d49bce914f84eef2fe59f9
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2012-10-25 22:17:18 +0200

    Fix flags issue between gcc and nvcc

--- cmake/FindCUDA_wrap.cmake
+++ cmake/FindCUDA_wrap.cmake
@@ -10,7 +10,7 @@ else ()
   # GCS 2011.03.16
   # Make nvcc less whiny
   if (CMAKE_COMPILER_IS_GNUCC)
-    set (CUDA_PROPAGATE_HOST_FLAGS OFF)
+    set (CUDA_PROPAGATE_HOST_FLAGS CACHE BOOL OFF)
   endif ()
 
   # GCS 2012-05-11:  We need to propagate cxx flags to nvcc, but 
@@ -19,7 +19,7 @@ else ()
     string (REPLACE "-ftest-coverage" "" TMP "${CMAKE_CXX_FLAGS}")
     string (REPLACE "-ftemplate-depth-50" "" TMP "${TMP}")
     string (REPLACE " " "," TMP "${TMP}")
-    set (CUDA_CXX_FLAGS ${TMP})
+    set (CUDA_CXX_FLAGS ${CUDA_CXX_FLAGS} ${TMP})
   endif ()
 
   # GCS 2012-05-07: Workaround for poor, troubled FindCUDA
