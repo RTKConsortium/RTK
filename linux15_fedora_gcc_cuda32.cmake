db6e0d72ebb29080ed44e406efa914341ca6a084
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2012-10-26 11:34:56 +0200

    Set all cuda versions on cluster

--- linux15_fedora_gcc_cuda32.cmake
+++ linux15_fedora_gcc_cuda32.cmake
@@ -1,11 +1,14 @@
 # client maintainer: simon.rit@creatis.insa-lyon.fr
-set(CTEST_SITE "mush.clb")
+set(CTEST_SITE "linux15.dg.creatis.insa-lyon.fr")
 set(CTEST_BUILD_NAME "Linux-64bit-gcc-cuda32")
 set(CTEST_BUILD_CONFIGURATION Release)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_DASHBOARD_ROOT "/tmp/RTK_dashboard")
 set(dashboard_binary_name "RTK_lin64_gcc_cuda32")
-set(ENV{ITK_DIR} "/home/srit/src/itk/lin64")
+set(ENV{ITK_DIR} "/home/srit/src/itk/lin64-dg")
 set(ENV{CUDA_BIN_PATH} "/home/srit/Download/cuda32/bin")
+set(ENV{PATH} "$ENV{PATH}:/home/srit/src/gcc/gcc436-install/bin")
+set(CTEST_BUILD_FLAGS -j8)
 
 include("${CTEST_SCRIPT_DIRECTORY}/rtk_common.cmake")
 
---
+++ linux15_lin64.sh
@@ -0,0 +1,6 @@
+ctest -S /home/srit/src/rtk/rtk-dashboard/linux15_fedora_gcc_cuda32.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/linux15_fedora_gcc_cuda40.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/linux15_fedora_gcc_cuda41.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/linux15_fedora_gcc_cuda42.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/linux15_fedora_gcc_cuda50.cmake -V
+
