3c4fc168c9411b79c0fd5427806da3f9fede76ad
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2012-11-06 23:33:47 +0100

    Moved code to generate elekta synergy geometry to specific filter

---
+++ code/rtkElektaSynergyGeometryReader.cxx
@@ -0,0 +1,112 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+
+#include "rtkElektaSynergyGeometryReader.h"
+#include "rtkDbf.h"
+
+rtk::ElektaSynergyGeometryReader
+::ElektaSynergyGeometryReader():
+  m_Geometry(NULL),
+  m_DicomUID(""),
+  m_ImageDbfFileName("IMAGE.DBF"),
+  m_FrameDbfFileName("FRAME.DBF")
+{
+  
+  GeometryType::Pointer m_Geometry;
+  std::string m_ImageDbfFileName;
+  std::string m_FrameDbfFileName;  
+}
+
+std::string
+rtk::ElektaSynergyGeometryReader
+::GetImageIDFromDicomUID()
+{
+  // Open frame database file
+  rtk::DbfFile dbImage(m_ImageDbfFileName);
+  if( !dbImage.is_open() )
+    itkGenericExceptionMacro( << "Couldn't open " 
+                              << m_ImageDbfFileName);
+
+  // Search for correct record
+  bool bReadOk;
+  do {
+    bReadOk = dbImage.ReadNextRecord();
+    }
+  while(bReadOk && std::string(m_DicomUID) != dbImage.GetFieldAsString("DICOM_UID") );
+
+  // Error message if not found
+  if(!bReadOk)
+    {
+    itkGenericExceptionMacro( << "Couldn't find acquisition with DICOM_UID "
+                              << m_DicomUID
+                              << " in table "
+                              << m_ImageDbfFileName );
+    }
+
+  return dbImage.GetFieldAsString("DBID");
+}
+
+void
+rtk::ElektaSynergyGeometryReader
+::GetProjInfoFromDB(const std::string &imageID,
+                    std::vector<float> &projAngle,
+                    std::vector<float> &projFlexX,
+                    std::vector<float> &projFlexY)
+{
+  // Open frame database file
+  rtk::DbfFile dbFrame(m_FrameDbfFileName);
+  if( !dbFrame.is_open() )
+    itkGenericExceptionMacro( << "Couldn't open " 
+                              << m_FrameDbfFileName);
+
+  // Go through the database, select correct records and get data
+  while( dbFrame.ReadNextRecord() )
+    {
+    if(dbFrame.GetFieldAsString("IMA_DBID") == imageID)
+      {
+      projAngle.push_back(dbFrame.GetFieldAsDouble("PROJ_ANG") );
+      projFlexX.push_back(dbFrame.GetFieldAsDouble("U_CENTRE") );
+      projFlexY.push_back(dbFrame.GetFieldAsDouble("V_CENTRE") );
+      }
+    }
+}
+
+void
+rtk::ElektaSynergyGeometryReader
+::GenerateData()
+{
+  // Create new RTK geometry object
+  m_Geometry = GeometryType::New();
+
+  // Get information from Synergy database
+  std::vector<float> projAngle, projFlexX, projFlexY;
+  GetProjInfoFromDB( GetImageIDFromDicomUID(), projAngle, projFlexX, projFlexY);
+
+  // Projection matrices
+  for(unsigned int noProj=0; noProj<projAngle.size(); noProj++)
+    {
+    m_Geometry->AddProjection(1000.,
+                            1536.,
+                            projAngle[noProj],
+                            -projFlexX[noProj],
+                            -projFlexY[noProj]);
+    }
+}
+
+
---
+++ code/rtkElektaSynergyGeometryReader.h
@@ -0,0 +1,95 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef __rtkElektaSynergyGeometryReader_h
+#define __rtkElektaSynergyGeometryReader_h
+
+#include <itkLightProcessObject.h>
+#include "rtkThreeDCircularProjectionGeometry.h"
+
+namespace rtk
+{
+
+/** \class ElektaSynergyGeometryReader
+ *
+ * Creates a 3D circular geometry from the Elekta database input. 
+ *
+ * \author Simon Rit
+ *
+ * \ingroup IOFilters
+ */
+class ElektaSynergyGeometryReader :
+  public itk::LightProcessObject
+{
+public:
+  /** Standard typedefs */
+  typedef ElektaSynergyGeometryReader Self;
+  typedef itk::LightProcessObject     Superclass;
+  typedef itk::SmartPointer<Self>     Pointer;
+
+  /** Convenient typedefs */
+  typedef ThreeDCircularProjectionGeometry GeometryType;
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(ElektaSynergyGeometryReader, LightProcessObject);
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Get the pointer to the generated geometry object. */
+  itkGetMacro(Geometry, GeometryType::Pointer);
+
+  /** Set the dicom unique ID of the acquisition, usually contained in the
+   * name of the directory containing the projection images. The name is
+   * of the form img_DicomUID. */
+  itkGetMacro(DicomUID, std::string);
+  itkSetMacro(DicomUID, std::string);
+
+  /** Set the path to the file IMAGE.DBF */
+  itkGetMacro(ImageDbfFileName, std::string);
+  itkSetMacro(ImageDbfFileName, std::string);
+
+  /** Set the path to the file FRAME.DBF */
+  itkGetMacro(FrameDbfFileName, std::string);
+  itkSetMacro(FrameDbfFileName, std::string);
+
+protected:
+  ElektaSynergyGeometryReader();
+
+
+private:
+  //purposely not implemented
+  ElektaSynergyGeometryReader(const Self&);
+  void operator=(const Self&);
+
+  std::string GetImageIDFromDicomUID();
+  void GetProjInfoFromDB(const std::string &imageID,
+                         std::vector<float> &projAngle,
+                         std::vector<float> &projFlexX,
+                         std::vector<float> &projFlexY);
+
+  virtual void GenerateData();
+
+  GeometryType::Pointer m_Geometry;
+  std::string m_DicomUID;
+  std::string m_ImageDbfFileName;
+  std::string m_FrameDbfFileName;  
+};
+
+}
+#endif
