67316d55509e98c58a1e085e678851b7055728b6
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2012-11-20 11:44:33 +0100

    Added ImagX image IO and raw to attenuation conversion

---
+++ code/rtkImagXLookupTableImageFilter.h
@@ -0,0 +1,97 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef __rtkImagXLookupTableImageFilter_h
+#define __rtkImagXLookupTableImageFilter_h
+
+#include "rtkLookupTableImageFilter.h"
+#include <itkNumericTraits.h>
+
+namespace rtk
+{
+
+/** \class ImagXLookupTableImageFilter
+ * \brief Lookup table for ImagX data.
+ *
+ * The lookup table converts the raw values measured by the panel to the
+ * logarithm of the value divided by the maximum numerical value. This could
+ * be improved with a calibration of the air value.
+ *
+ * \author Simon Rit
+ *
+ * \ingroup ImageToImageFilter
+ */
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT ImagXLookupTableImageFilter : public LookupTableImageFilter<TInputImage, TOutputImage>
+{
+
+public:
+  /** Standard class typedefs. */
+  typedef ImagXLookupTableImageFilter                       Self;
+  typedef LookupTableImageFilter<TInputImage, TOutputImage> Superclass;
+  typedef itk::SmartPointer<Self>                           Pointer;
+  typedef itk::SmartPointer<const Self>                     ConstPointer;
+
+  typedef typename TInputImage::PixelType                   InputImagePixelType;
+  typedef typename TOutputImage::PixelType                  OutputImagePixelType;
+  typedef typename Superclass::FunctorType::LookupTableType LookupTableType;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Runtime information support. */
+  itkTypeMacro(ImagXLookupTableImageFilter, LookupTableImageFilter);
+protected:
+  ImagXLookupTableImageFilter();
+  virtual ~ImagXLookupTableImageFilter() {
+  }
+
+private:
+  ImagXLookupTableImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&);              //purposely not implemented
+
+};
+
+} // end namespace rtk
+
+template <class TInputImage, class TOutputImage>
+rtk::ImagXLookupTableImageFilter<TInputImage, TOutputImage>::ImagXLookupTableImageFilter()
+{
+  // Create the lut
+  typename LookupTableType::Pointer lut = LookupTableType::New();
+  typename LookupTableType::SizeType size;
+  size[0] = itk::NumericTraits<InputImagePixelType>::max()-itk::NumericTraits<InputImagePixelType>::min()+1;
+  lut->SetRegions( size );
+  lut->Allocate();
+
+  OutputImagePixelType logRef = log(OutputImagePixelType(size[0]) );
+
+  // Iterate and set lut
+  itk::ImageRegionIteratorWithIndex<LookupTableType> it( lut, lut->GetBufferedRegion() );
+  it.GoToBegin();
+  while( !it.IsAtEnd() )
+    {
+    it.Set( logRef - log(it.GetIndex()[0]+1.) );
+    ++it;
+    }
+
+  // Set the lut to member and functor
+  this->SetLookupTable(lut);
+}
+
+#endif
---
+++ code/rtkImagXXMLFileReader.cxx
@@ -0,0 +1,109 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "rtkImagXXMLFileReader.h"
+#include "itkMacro.h"
+
+#include <itksys/SystemTools.hxx>
+#include <itkMetaDataObject.h>
+
+namespace rtk
+{
+
+int
+ImagXXMLFileReader::
+CanReadFile(const char *name)
+{
+  if(!itksys::SystemTools::FileExists(name) ||
+     itksys::SystemTools::FileIsDirectory(name) ||
+     itksys::SystemTools::FileLength(name) == 0)
+    return 0;
+  return 1;
+}
+
+void
+ImagXXMLFileReader::
+StartElement(const char * name, const char ** atts)
+{
+#define ENCAPLULATE_META_DATA_INT(metaName) \
+  if(itksys::SystemTools::Strucmp(atts[i], metaName) == 0) { \
+    double d = atof(atts[i+1]); \
+    itk::EncapsulateMetaData<int>(m_Dictionary, metaName, d); \
+    }
+
+#define ENCAPLULATE_META_DATA_STRING(metaName) \
+  if(itksys::SystemTools::Strucmp(atts[i], metaName) == 0) { \
+    itk::EncapsulateMetaData<std::string>(m_Dictionary, metaName, atts[i+1]); \
+    }
+
+  if(std::string(name) == std::string("image") )
+    {
+    for(int i=0; atts[i] != NULL; i+=2)
+      {
+      ENCAPLULATE_META_DATA_STRING("name");
+      ENCAPLULATE_META_DATA_INT("bitDepth");
+      ENCAPLULATE_META_DATA_STRING("pixelFormat");
+      ENCAPLULATE_META_DATA_STRING("byteOrder");
+      ENCAPLULATE_META_DATA_STRING("modality");
+      ENCAPLULATE_META_DATA_STRING("matrixTransform");
+      ENCAPLULATE_META_DATA_INT("dimensions");
+      ENCAPLULATE_META_DATA_INT("sequence");
+      ENCAPLULATE_META_DATA_STRING("rawFile");
+      }
+    }
+  if(std::string(name) == std::string("size") )
+    {
+    for(int i=0; atts[i] != NULL; i+=2)
+      {
+      ENCAPLULATE_META_DATA_INT("x");
+      ENCAPLULATE_META_DATA_INT("y");
+      ENCAPLULATE_META_DATA_INT("z");
+      }
+    }
+  if(std::string(name) == std::string("spacing") )
+    {
+#define ENCAPLULATE_META_DATA_DOUBLE(metaName) \
+  if(itksys::SystemTools::Strucmp(atts[i], metaName) == 0) { \
+    double d = atof(atts[i+1]); \
+    itk::EncapsulateMetaData<double>(m_Dictionary, std::string("spacing_") + std::string(metaName), d); \
+    }
+    for(int i=0; atts[i] != NULL; i+=2)
+      {
+      ENCAPLULATE_META_DATA_DOUBLE("x");
+      ENCAPLULATE_META_DATA_DOUBLE("y");
+      ENCAPLULATE_META_DATA_DOUBLE("z");
+      }
+    }
+  m_CurCharacterData = "";
+}
+
+void
+ImagXXMLFileReader::
+EndElement(const char *name)
+{
+}
+
+void
+ImagXXMLFileReader::
+CharacterDataHandler(const char *inData, int inLength)
+{
+  for(int i = 0; i < inLength; i++)
+    m_CurCharacterData = m_CurCharacterData + inData[i];
+}
+
+}
