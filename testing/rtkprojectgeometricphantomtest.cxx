1d884930dd320b6b8be1d22b28011e1ccee8af8a
Marc Vila <marc.vilaoliva@uclouvain.be>
2012-11-26 15:28:14 +0100

    TESTING: two new tests.

---
+++ testing/rtkprojectgeometricphantomtest.cxx
@@ -0,0 +1,122 @@
+#include "rtkTestConfiguration.h"
+#include "rtkMacro.h"
+#include "rtkThreeDCircularProjectionGeometryXMLFile.h"
+#include "rtkConstantImageSource.h"
+#include "rtkGeometricPhantomFileReader.h"
+#include "rtkProjectGeometricPhantomImageFilter.h"
+#include "rtkSheppLoganPhantomFilter.h"
+
+#include <itkRegularExpressionSeriesFileNames.h>
+
+typedef rtk::ThreeDCircularProjectionGeometry GeometryType;
+
+template<class TImage>
+void CheckImageQuality(typename TImage::Pointer recon, typename TImage::Pointer ref)
+{
+  typedef itk::ImageRegionConstIterator<TImage> ImageIteratorType;
+  ImageIteratorType itTest( recon, recon->GetBufferedRegion() );
+  ImageIteratorType itRef( ref, ref->GetBufferedRegion() );
+
+  typedef double ErrorType;
+  ErrorType TestError = 0.;
+  ErrorType EnerError = 0.;
+
+  itTest.GoToBegin();
+  itRef.GoToBegin();
+
+  while( !itRef.IsAtEnd() )
+    {
+    typename TImage::PixelType TestVal = itTest.Get();
+    typename TImage::PixelType RefVal = itRef.Get();
+
+    if( TestVal != RefVal )
+      {
+      TestError += vcl_abs(RefVal - TestVal);
+      EnerError += vcl_pow(ErrorType(RefVal - TestVal), 2.);
+      }
+    ++itTest;
+    ++itRef;
+    }
+  // Error per Pixel
+  ErrorType ErrorPerPixel = TestError/recon->GetBufferedRegion().GetNumberOfPixels();
+  std::cout << "\nError per Pixel = " << ErrorPerPixel << std::endl;
+  // MSE
+  ErrorType MSE = EnerError/ref->GetBufferedRegion().GetNumberOfPixels();
+  std::cout << "MSE = " << MSE << std::endl;
+  // PSNR
+  ErrorType PSNR = 20*log10(255.0) - 10*log10(MSE);
+  std::cout << "PSNR = " << PSNR << "dB" << std::endl;
+  // QI
+  ErrorType QI = (255.0-ErrorPerPixel)/255.0;
+  std::cout << "QI = " << QI << std::endl;
+
+  // Checking results
+  if (ErrorPerPixel > 0.00055)
+    {
+    std::cerr << "Test Failed, Error per pixel not valid! "
+              << ErrorPerPixel << " instead of 0.00055" << std::endl;
+    exit( EXIT_FAILURE);
+    }
+  if (PSNR < 88.)
+    {
+    std::cerr << "Test Failed, PSNR not valid! "
+              << PSNR << " instead of 88" << std::endl;
+    exit( EXIT_FAILURE);
+    }
+}
+
+int main(int, char** )
+{
+    const unsigned int Dimension = 3;
+    typedef float                                    OutputPixelType;
+    typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
+    const unsigned int NumberOfProjectionImages = 180;
+
+    // Constant image sources
+    typedef rtk::ConstantImageSource< OutputImageType > ConstantImageSourceType;
+    ConstantImageSourceType::PointType origin;
+    ConstantImageSourceType::SizeType size;
+    ConstantImageSourceType::SpacingType spacing;
+
+    ConstantImageSourceType::Pointer projectionsSource = ConstantImageSourceType::New();
+    origin[0] = -254.;
+    origin[1] = -254.;
+    origin[2] = -254.;
+    size[0] = 128;
+    size[1] = 128;
+    size[2] = NumberOfProjectionImages;
+    spacing[0] = 4.;
+    spacing[1] = 4.;
+    spacing[2] = 4.;
+    projectionsSource->SetOrigin( origin );
+    projectionsSource->SetSpacing( spacing );
+    projectionsSource->SetSize( size );
+    projectionsSource->SetConstant( 0. );
+
+    // Geometry object
+    typedef rtk::ThreeDCircularProjectionGeometry GeometryType;
+    GeometryType::Pointer geometry = GeometryType::New();
+    for(unsigned int noProj=0; noProj<NumberOfProjectionImages; noProj++)
+      geometry->AddProjection(600., 1200., noProj*360./NumberOfProjectionImages);
+
+    // Shepp Logan projections filter
+    typedef rtk::SheppLoganPhantomFilter<OutputImageType, OutputImageType> SLPType;
+    SLPType::Pointer slp=SLPType::New();
+    slp->SetInput( projectionsSource->GetOutput() );
+    slp->SetGeometry(geometry);
+    TRY_AND_EXIT_ON_ITK_EXCEPTION( slp->Update() );
+
+    // Shepp Logan projections filter from Configuration File
+    typedef rtk::ProjectGeometricPhantomImageFilter<OutputImageType, OutputImageType> PGPType;
+    PGPType::Pointer pgp=PGPType::New();
+    pgp->SetInput( projectionsSource->GetOutput() );
+    pgp->SetGeometry(geometry);
+    pgp->SetConfigFile(std::string(RTK_DATA_ROOT) +
+                       std::string("/Input/GeometricPhantom/SheppLogan.txt"));
+    TRY_AND_EXIT_ON_ITK_EXCEPTION( pgp->Update() );
+
+    CheckImageQuality<OutputImageType>(slp->GetOutput(), pgp->GetOutput());
+    std::cout << "Test PASSED! " << std::endl;
+
+    return EXIT_SUCCESS;
+}
