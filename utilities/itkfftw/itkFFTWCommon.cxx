b1c800c2f72d83b7898cf9ccf7d681add4357a36
Julien Jomier <julien.jomier@kitware.com>
2012-11-29 20:05:27 +0100

    ENH: Adding FFTW configuration from ITKv4

--- utilities/itkfftw/itkFFTWCommon.cxx
+++
@@ -1,9 +0,0 @@
-#include "itkFFTWCommon.h"
-
-#if defined(USE_FFTWF)
-itk::FastMutexLock::Pointer itk::fftw::Proxy<float>::lock = NULL;
-#endif
-
-#if defined(USE_FFTWD)
-itk::FastMutexLock::Pointer itk::fftw::Proxy<double>::lock = NULL;
-#endif
