54caa21293d09f5f48d067ba1c4afb082b4d8299
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2013-02-21 14:56:48 +0100

    Added valgrind test

---
+++ linux15_fedora_gcc_cuda_system_valgrind.cmake
@@ -0,0 +1,25 @@
+# client maintainer: simon.rit@creatis.insa-lyon.fr
+set(CTEST_SITE "linux15.dg.creatis.insa-lyon.fr")
+set(CTEST_BUILD_NAME "Linux-64bit-gcc-cuda-system-valgrind")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_DASHBOARD_ROOT "/tmp/RTK_dashboard")
+set(dashboard_binary_name "RTK_lin64_gcc_cuda_system-valgrind")
+set(ENV{ITK_DIR} "/home/srit/src/itk/lin64-dg")
+set(ENV{CUDA_BIN_PATH} "/usr/lib64/cuda/bin")
+set(CTEST_BUILD_FLAGS -j8)
+
+# OpenCL
+set(ENV{LD_LIBRARY_PATH} "/usr/lib64/nvidia:$ENV{LD_LIBRARY_PATH}")
+set(CONFIGURE_OPTIONS "-DOPENCL_ROOT_DIR=/usr/lib64/cuda")
+
+SET(ENV{VALGRIND_LIB} "/usr/lib64/valgrind")
+SET(CTEST_MEMORYCHECK_COMMAND /usr/bin/valgrind)
+SET(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--gen-suppressions=all --child-silent-after-fork=yes -q --leak-check=yes --show-reachable=yes --workaround-gcc296-bugs=yes --num-callers=50 -v")
+SET(CONFIGURE_OPTIONS ${CONFIGURE_OPTIONS}
+                      -DFAST_TESTS_NO_CHECKS=TRUE
+                      -DMEMORYCHECK_COMMAND=${CTEST_MEMORYCHECK_COMMAND})
+set(dashboard_do_memcheck true)
+
+include("${CTEST_SCRIPT_DIRECTORY}/rtk_common.cmake")
+
