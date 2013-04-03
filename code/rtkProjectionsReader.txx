5a4bd0db2e1661a24a1d2f240b656287fb7b84e7
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2013-04-03 12:00:49 +0200

    Added first version of XRad geometry and projections processing

--- code/rtkProjectionsReader.txx
+++ code/rtkProjectionsReader.txx
@@ -42,6 +42,10 @@
 #include "rtkEdfImageIOFactory.h"
 #include "rtkEdfRawToAttenuationImageFilter.h"
 
+// Xrad small animal scanner
+#include "rtkXRadImageIOFactory.h"
+#include "rtkXRadRawToAttenuationImageFilter.h"
+
 namespace rtk
 {
 
@@ -71,6 +75,7 @@ void ProjectionsReader<TOutputImage>
     rtk::HisImageIOFactory::RegisterOneFactory();
     rtk::ImagXImageIOFactory::RegisterOneFactory();
     rtk::EdfImageIOFactory::RegisterOneFactory();
+    rtk::XRadImageIOFactory::RegisterOneFactory();
 #if ITK_VERSION_MAJOR <= 3
     itk::ImageIOFactory::RegisterBuiltInFactories();
 #endif
@@ -174,6 +179,25 @@ void ProjectionsReader<TOutputImage>
       typename RawFilterType::Pointer rawFilter = RawFilterType::New();
       rawFilter->SetInput( reader->GetOutput() );
       rawFilter->SetFileNames( this->GetFileNames() );
+      m_RawToProjectionsFilter = rawFilter;
+      }
+    else if( !strcmp(imageIO->GetNameOfClass(), "XRadImageIO") )
+      {
+      /////////// XRad
+      typedef unsigned short                                     InputPixelType;
+      typedef itk::Image< InputPixelType, OutputImageDimension > InputImageType;
+
+      // Reader
+      typedef itk::ImageSeriesReader< InputImageType > ReaderType;
+      typename ReaderType::Pointer reader = ReaderType::New();
+      reader->SetImageIO( imageIO );
+      reader->SetFileNames( this->GetFileNames() );
+      m_RawDataReader = reader;
+
+      // Convert raw to Projections
+      typedef rtk::XRadRawToAttenuationImageFilter<InputImageType, OutputImageType> RawFilterType;
+      typename RawFilterType::Pointer rawFilter = RawFilterType::New();
+      rawFilter->SetInput( reader->GetOutput() );
       m_RawToProjectionsFilter = rawFilter;
       }
     else
---
+++ code/rtkXRadGeometryReader.cxx
@@ -0,0 +1,113 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+
+#include "rtkXRadGeometryReader.h"
+#include "rtkXRadImageIOFactory.h"
+
+#include <itkImageIOFactory.h>
+#include <itkMetaDataObject.h>
+
+rtk::XRadGeometryReader
+::XRadGeometryReader():
+  m_Geometry(NULL)
+{
+}
+
+void
+rtk::XRadGeometryReader
+::GenerateData()
+{
+  // Create new RTK geometry object
+  m_Geometry = GeometryType::New();
+  GeometryType::Pointer tmpGeo = GeometryType::New();
+
+  // Read image information which contains geometry information
+  rtk::XRadImageIOFactory::RegisterOneFactory();
+  itk::ImageIOBase::Pointer reader =
+        itk::ImageIOFactory::CreateImageIO(m_ImageFileName.c_str(), itk::ImageIOFactory::ReadMode);
+  if(!reader)
+    itkExceptionMacro(<< m_ImageFileName << " is not an XRad file.");
+  reader->SetFileName(m_ImageFileName);
+  reader->ReadImageInformation();
+
+  std::string sectionName, paramName;
+  typedef itk::MetaDataObject< std::string > MetaDataStringType;
+  itk::MetaDataDictionary &dic = reader->GetMetaDataDictionary();
+
+  for(unsigned int i=0; i<reader->GetDimensions(2); i++)
+    {
+    std::ostringstream os;
+    os << "iView3D(Projection " << i << ")";
+    sectionName = os.str();
+
+    paramName = sectionName + "_CBCT.ProjectionGeometryArray.u_axis";
+    std::istringstream isu(dynamic_cast<MetaDataStringType*>(dic[paramName].GetPointer())->GetMetaDataObjectValue());
+    paramName = sectionName + "_CBCT.ProjectionGeometryArray.v_axis";
+    std::istringstream isv(dynamic_cast<MetaDataStringType*>(dic[paramName].GetPointer())->GetMetaDataObjectValue());
+    paramName = sectionName + "_CBCT.ProjectionGeometryArray.focus";
+    std::istringstream isfocus(dynamic_cast<MetaDataStringType*>(dic[paramName].GetPointer())->GetMetaDataObjectValue());
+    paramName = sectionName + "_CBCT.ProjectionGeometryArray.center";
+    std::istringstream iscenter(dynamic_cast<MetaDataStringType*>(dic[paramName].GetPointer())->GetMetaDataObjectValue());
+    itk::Vector<double,3> u,v;
+    itk::Vector<double,3> focus,center;
+    for(unsigned int j=0; j<3; j++)
+      {
+      isu >> u[j];
+      isv >> v[j];
+      isfocus >> focus[j];
+      iscenter >> center[j];
+
+      // cm to mm
+      focus[j] *= 10.;
+      center[j] *= 10.;
+      }
+
+    // Change of coordinate system to IEC
+    // The coordinate system of XRad is supposed to be illustrated in figure 1 of
+    // [Clarkson et al, Med Phys, 2011]. y and z are swapped, the rest is not clear.
+    std::swap(u[1], u[2]);
+    std::swap(v[1], v[2]);
+    std::swap(focus[1], focus[2]);
+    std::swap(center[1], center[2]);
+
+    u.Normalize();
+    v.Normalize();
+    tmpGeo->AddReg23Projection(&(focus[0]),
+                               &(center[0]),
+                               u, v);
+
+    paramName = sectionName + "_CBCT.ProjectionGeometryArray.u_off";
+    std::string suoff = dynamic_cast<MetaDataStringType*>(dic[paramName].GetPointer())->GetMetaDataObjectValue();
+    double uoff = atof(suoff.c_str()) * reader->GetSpacing(0);
+
+    paramName = sectionName + "_CBCT.ProjectionGeometryArray.v_off";
+    std::string svoff = dynamic_cast<MetaDataStringType*>(dic[paramName].GetPointer())->GetMetaDataObjectValue();
+    double voff = atof(svoff.c_str()) * reader->GetSpacing(1);
+
+    m_Geometry->AddProjection(tmpGeo->GetSourceToIsocenterDistances()[i],
+                              tmpGeo->GetSourceToDetectorDistances()[i],
+                              tmpGeo->GetGantryAngles()[i],
+                              tmpGeo->GetProjectionOffsetsX()[i]-uoff,
+                              tmpGeo->GetProjectionOffsetsY()[i]-voff,
+                              tmpGeo->GetOutOfPlaneAngles()[i],
+                              tmpGeo->GetInPlaneAngles()[i],
+                              tmpGeo->GetSourceOffsetsX()[i],
+                              tmpGeo->GetSourceOffsetsY()[i]);
+    }
+}
