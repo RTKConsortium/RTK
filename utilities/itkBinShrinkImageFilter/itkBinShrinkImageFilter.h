e5871bcb663b406bbc6b7b07a4b549ab590ecff4
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2013-12-18 10:56:30 +0100

    Added binning filter and required dependency from Insight journal

---
+++ utilities/itkBinShrinkImageFilter/itkBinShrinkImageFilter.h
@@ -0,0 +1,133 @@
+/*=========================================================================
+ *
+ *  Copyright Bradley Lowekamp
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __itkBinShrinkImageFilter_h
+#define __itkBinShrinkImageFilter_h
+
+#include "itkShrinkImageFilter.h"
+
+namespace itk
+{
+
+/** \class BinShrinkImageFilter
+ * \brief Reduce the size of an image by an integer factor in each
+ * dimension and averages by the same size.
+ *
+ * BinShrinkImageFilter reduces the size of an image by an integer factor
+ * in each dimension. The algorithm implemented is a mean or box
+ * filter subsample.
+ *
+ * The output image size in each dimension is given by:
+ *
+ * outputSize[j] = max( vcl_floor(inputSize[j]/shrinkFactor[j]), 1 );
+ *
+ * This filter is implemented so that the starting extent of the first
+ * pixel of the output matches that of the input.
+ *
+ * \ingroup ITKBinShrink
+ * \ingroup Streamed
+ */
+template <class TInputImage, class TOutputImage>
+class ITK_EXPORT BinShrinkImageFilter:
+    public ImageToImageFilter<TInputImage,TOutputImage>
+{
+public:
+  /** Standard class typedefs. */
+  typedef BinShrinkImageFilter                          Self;
+  typedef ImageToImageFilter<TInputImage,TOutputImage>   Superclass;
+  typedef SmartPointer<Self>                            Pointer;
+  typedef SmartPointer<const Self>                      ConstPointer;
+
+  /** Method for creation through the object factory. */
+  itkNewMacro(Self);
+
+  /** Run-time type information (and related methods). */
+  itkTypeMacro(BinShrinkImageFilter, ShrinkImageFilter);
+
+  /** Typedef to images */
+  typedef TOutputImage                                OutputImageType;
+  typedef TInputImage                                 InputImageType;
+  typedef typename OutputImageType::Pointer           OutputImagePointer;
+  typedef typename InputImageType::Pointer            InputImagePointer;
+  typedef typename InputImageType::ConstPointer       InputImageConstPointer;
+
+  typedef typename TOutputImage::OffsetType           OutputOffsetType;
+  typedef typename TOutputImage::IndexType            OutputIndexType;
+  typedef typename TInputImage::IndexType             InputIndexType;
+
+  /** Typedef to describe the output image region type. */
+  typedef typename TOutputImage::RegionType OutputImageRegionType;
+
+
+  /** ImageDimension enumeration. */
+  itkStaticConstMacro(ImageDimension, unsigned int,
+                      TInputImage::ImageDimension );
+  itkStaticConstMacro(OutputImageDimension, unsigned int,
+                      TOutputImage::ImageDimension );
+
+  typedef FixedArray< unsigned int, ImageDimension > ShrinkFactorsType;
+
+  /** Set the shrink factors. Values are clamped to
+   * a minimum value of 1. Default is 1 for all dimensions. */
+  itkSetMacro(ShrinkFactors, ShrinkFactorsType);
+  void SetShrinkFactors(unsigned int factor);
+  void SetShrinkFactor(unsigned int i, unsigned int factor);
+
+  /** Get the shrink factors. */
+  itkGetConstReferenceMacro(ShrinkFactors, ShrinkFactorsType);
+
+  virtual void GenerateOutputInformation();
+
+  /** BinShrinkImageFilter needs a larger input requested region than the output
+   * requested region.  As such, BinShrinkImageFilter needs to provide an
+   * implementation for GenerateInputRequestedRegion() in order to inform the
+   * pipeline execution model.
+   * \sa ProcessObject::GenerateInputRequestedRegion() */
+  virtual void GenerateInputRequestedRegion();
+
+
+#ifdef ITK_USE_CONCEPT_CHECKING
+  /** Begin concept checking */
+  itkConceptMacro(InputConvertibleToOutputCheck,
+    (Concept::Convertible<typename TInputImage::PixelType, typename TOutputImage::PixelType>));
+  itkConceptMacro(SameDimensionCheck,
+    (Concept::SameDimension<ImageDimension, OutputImageDimension>));
+  /** End concept checking */
+#endif
+
+protected:
+  BinShrinkImageFilter();
+  void PrintSelf(std::ostream & os, Indent indent) const;
+
+  void ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread,
+                            ThreadIdType threadId );
+
+private:
+  BinShrinkImageFilter(const Self&); //purposely not implemented
+  void operator=(const Self&); //purposely not implemented
+
+  ShrinkFactorsType m_ShrinkFactors;
+
+};
+
+} // end namespace itk
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "itkBinShrinkImageFilter.hxx"
+#endif
+
+#endif
