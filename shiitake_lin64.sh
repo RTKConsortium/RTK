56aa8ad720c63319e0c683f878713556b89d7eb5
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2014-04-24 21:25:15 +0200

    Disable intel and opencl compilations

--- shiitake_lin64.sh
+++ shiitake_lin64.sh
@@ -2,8 +2,6 @@
 
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_debug.cmake -V
-ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_gcc_opencl.cmake -V
-ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_suse_intel.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_style.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/shiitake_doxygen.cmake -V
 rsync -e 'ssh -i /home/srit/.ssh/nophrase' -a --delete \
