b75c3a2b20fb1bf0521d280e333d40fc07d67e74
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2014-07-04 17:01:16 +0200

    New versions of ITK

---
+++ linux15_fedora_gcc_cuda_system_itk450.cmake
@@ -0,0 +1,23 @@
+# client maintainer: simon.rit@creatis.insa-lyon.fr
+set(CTEST_SITE "linux15.dg.creatis.insa-lyon.fr")
+set(CTEST_BUILD_NAME "Linux-64bit-gcc-cuda-system-itk450")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_DASHBOARD_ROOT "/tmp/RTK_dashboard")
+set(dashboard_binary_name "RTK_lin64_gcc_cuda_system_itk450")
+set(ENV{ITK_DIR} "/home/srit/src/itk450/lin64-dg/lib/cmake/ITK-4.5")
+set(ENV{CUDA_BIN_PATH} "/usr/lib64/cuda/bin")
+set(ENV{CUDA_LIB_PATH} "/usr/lib64/nvidia-304xx")
+set(CTEST_BUILD_FLAGS -j8)
+
+# OpenCL
+set(ENV{LD_LIBRARY_PATH} "/usr/lib64/nvidia:$ENV{LD_LIBRARY_PATH}")
+set(CONFIGURE_OPTIONS
+   -DOPENCL_LIBRARIES:PATH=/usr/lib64/nvidia-304xx/libOpenCL.so.1
+   -DOPENCL_INCLUDE_DIRS:PATH=/usr/include/cuda
+   -DRTK_USE_CUDA:BOOL=ON
+   -DRTK_USE_OPENCL:BOOL=ON
+  )
+
+include("${CTEST_SCRIPT_DIRECTORY}/rtk_common.cmake")
+
