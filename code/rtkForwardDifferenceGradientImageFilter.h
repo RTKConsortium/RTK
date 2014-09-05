8823aa27ec30a3f92cab5ce5b8e267d2270cef51
Cyril Mory <cyril.mory@creatis.insa-lyon.fr>
2014-09-05 11:15:16 +0200

    Cosmetics

--- code/rtkForwardDifferenceGradientImageFilter.h
+++ code/rtkForwardDifferenceGradientImageFilter.h
@@ -83,11 +83,11 @@ public:
   itkTypeMacro(ForwardDifferenceGradientImageFilter, ImageToImageFilter);
 
   /** Image typedef support. */
-  typedef typename InputImageType::PixelType  InputPixelType;
-  typedef TOperatorValueType                  OperatorValueType;
-  typedef TOuputValue                         OutputValueType;
-  typedef typename OutputImageType::PixelType CovariantVectorType;
-  typedef typename OutputImageType::RegionType OutputImageRegionType;
+  typedef typename InputImageType::PixelType  	InputPixelType;
+  typedef TOperatorValueType                  	OperatorValueType;
+  typedef TOuputValue                         	OutputValueType;
+  typedef typename OutputImageType::PixelType 	CovariantVectorType;
+  typedef typename OutputImageType::RegionType 	OutputImageRegionType;
 
   /** ForwardDifferenceGradientImageFilter needs a larger input requested region than
    * the output requested region.  As such, ForwardDifferenceGradientImageFilter needs
