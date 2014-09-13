ccf4e44c3ce7bd88b34f446634375c2ea17072b5
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2014-09-13 18:13:34 +0200

    Fixed script used to compile difference itk versions

--- build_itk_gcc.sh
+++ build_itk_gcc.sh
@@ -1,14 +1,15 @@
 if test $# -gt 0
+then
 	export CC=/home/srit/src/gcc/gcc${1}${2}${3}-install/bin/gcc
 	export CXX=/home/srit/src/gcc/gcc${1}${2}${3}-install/bin/c++
 	export LD_LIBRARY_PATH=/home/srit/src/gcc/gcc${1}${2}${3}-install/lib64:$LD_LIBRARY_PATH 
 	export LD_LIBRARY_PATH=/home/srit/src/gcc/gcc${1}${2}${3}-install/lib:$LD_LIBRARY_PATH 
 	export PATH=/home/srit/src/gcc/gcc${1}${2}${3}-install/bin:$PATH 
-	mkdir $(dirname $0)/lin64_gcc_${1}${2}${3}
-	cd $(dirname $0)/lin64_gcc_${1}${2}${3}/
+	mkdir /home/srit/src/itk/lin64_gcc_${1}${2}${3}
+	cd /home/srit/src/itk/lin64_gcc_${1}${2}${3}/
 else
-	mkdir $(dirname $0)/lin64-dg
-	cd $(dirname $0)/lin64-dg
+	mkdir /home/srit/src/itk/lin64-dg
+	cd /home/srit/src/itk/lin64-dg
 fi
 cmake ../itk -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF -DModule_ITKReview=ON -DITK_USE_FFTWD=ON -DITK_USE_FFTWF=ON -DITK_USE_SYSTEM_FFTW=ON
 make -j18
