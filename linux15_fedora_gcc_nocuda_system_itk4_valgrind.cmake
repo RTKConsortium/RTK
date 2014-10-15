a251310ff23cec0949c908b46de34d5f7737259c
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2014-10-15 08:57:36 +0200

    Added and use new valgrind suppresion file, with and without RTK_USE_CUDA

---
+++ linux15_fedora_gcc_nocuda_system_itk4_valgrind.cmake
@@ -0,0 +1,20 @@
+# client maintainer: simon.rit@creatis.insa-lyon.fr
+set(CTEST_SITE "linux15.dg.creatis.insa-lyon.fr")
+set(CTEST_BUILD_NAME "Linux-64bit-gcc-nocuda-system-itk4-valgrind")
+set(CTEST_BUILD_CONFIGURATION RelWithDebInfo)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_DASHBOARD_ROOT "/tmp/RTK_dashboard")
+set(dashboard_binary_name "RTK_lin64_gcc_nocuda_system_itk4_valgrind")
+set(ENV{ITK_DIR} "/home/srit/src/itk452-RelWithDebInfo/lin64-dg")
+set(CTEST_BUILD_FLAGS -j8)
+
+SET(ENV{VALGRIND_LIB} "/usr/lib64/valgrind")
+SET(CTEST_MEMORYCHECK_COMMAND /usr/bin/valgrind)
+SET(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--gen-suppressions=all --child-silent-after-fork=yes -q --leak-check=yes --show-reachable=yes --workaround-gcc296-bugs=yes --num-callers=50 -v")
+SET(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE "${CTEST_SCRIPT_DIRECTORY}/RTK.supp")
+SET(CONFIGURE_OPTIONS -DRTK_USE_CUDA=FALSE 
+                      -DFAST_TESTS_NO_CHECKS=TRUE)
+set(dashboard_do_memcheck true)
+
+include("${CTEST_SCRIPT_DIRECTORY}/rtk_common.cmake")
+
