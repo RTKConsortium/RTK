415756016d25edaf0fe999a46f3b5ad231b0e032
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2014-10-22 18:26:33 +0200

    Added streaming options to a few cuda tests and fixed bug in CUDA displacement filter

--- testing/rtkdisplaceddetectorcompcudatest.cxx
+++ testing/rtkdisplaceddetectorcompcudatest.cxx
@@ -5,6 +5,10 @@
 #include "rtkSheppLoganPhantomFilter.h"
 
 #include <itkStreamingImageFilter.h>
+#if ITK_VERSION_MAJOR > 4 || (ITK_VERSION_MAJOR == 4 && ITK_VERSION_MINOR >= 4)
+  #include <itkImageRegionSplitterDirection.h>
+#endif
+
 
 /**
  * \file rtkdisplaceddetectorcompcudatest.cxx
@@ -48,10 +52,10 @@ int main(int, char** )
   // Geometry
   typedef rtk::ThreeDCircularProjectionGeometry GeometryType;
   GeometryType::Pointer geometry = GeometryType::New();
-  geometry->AddProjection(600., 0., 0, 84., 35, 23, 15, 21, 26);
-  geometry->AddProjection(500., 45., 0, 21., 12, 16, 546, 14, 41);
-  geometry->AddProjection(700., 90., 0, 68., 68, 54, 38, 35, 56);
-  geometry->AddProjection(900., 135., 0, 48., 35, 84, 10, 84, 59);
+  geometry->AddProjection(600., 700., 0.  , 84., 35, 23, 15, 21, 26);
+  geometry->AddProjection(500., 800., 45. , 21., 12, 16, 546, 14, 41);
+  geometry->AddProjection(700., 900., 90. , 68., 68, 54, 38, 35, 56);
+  geometry->AddProjection(900., 1000., 135., 48., 35, 84, 10, 84, 59);
 
   // Projections
   typedef rtk::SheppLoganPhantomFilter<OutputImageType, OutputImageType> SLPType;
@@ -82,7 +86,7 @@ int main(int, char** )
     cpuddf->InPlaceOff();
     TRY_AND_EXIT_ON_ITK_EXCEPTION( cpuddf->Update() );
 
-    CheckImageQuality< OutputImageType >(cudaddf->GetOutput(), cpuddf->GetOutput(), 1.e-6, 100, 1.);
+    CheckImageQuality< OutputImageType >(cudaddf->GetOutput(), cpuddf->GetOutput(), 1.e-5, 100, 1.);
 
     std::cout << "\n\n****** Case " << inPlace*2+1 << ": with streaming, ";
     if(!inPlace)
@@ -98,7 +102,14 @@ int main(int, char** )
     typedef itk::StreamingImageFilter<OutputImageType, OutputImageType> StreamingType;
     StreamingType::Pointer streamingCUDA = StreamingType::New();
     streamingCUDA->SetInput( cudaddf->GetOutput() );
+#if ITK_VERSION_MAJOR > 4 || (ITK_VERSION_MAJOR == 4 && ITK_VERSION_MINOR >= 4)
+    streamingCUDA->SetNumberOfStreamDivisions(4);
+    itk::ImageRegionSplitterDirection::Pointer splitter = itk::ImageRegionSplitterDirection::New();
+    splitter->SetDirection(2);
+    streamingCUDA->SetRegionSplitter(splitter);
+#else
     streamingCUDA->SetNumberOfStreamDivisions(2);
+#endif
     TRY_AND_EXIT_ON_ITK_EXCEPTION( streamingCUDA->Update() );
 
     cpuddf = CPUDDFType::New();
@@ -111,7 +122,7 @@ int main(int, char** )
     streamingCPU->SetNumberOfStreamDivisions(2);
     TRY_AND_EXIT_ON_ITK_EXCEPTION( streamingCPU->Update() );
 
-    CheckImageQuality< OutputImageType >(streamingCUDA->GetOutput(), streamingCPU->GetOutput(), 1.e-6, 100, 1.);
+    CheckImageQuality< OutputImageType >(streamingCUDA->GetOutput(), streamingCPU->GetOutput(), 1.e-5, 100, 1.);
     }
 
   // If all succeed
