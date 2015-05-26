6188662e927c4aabca6ee8fb0f95fd36e3b9ab32
Cyril Mory <you@example.com>
2015-05-26 11:42:04 +0200

    Modified the cuda splat and interpolation filters to use cublas Modified the input requested region in interpolation filter

--- code/rtkCudaSplatImageFilter.cu
+++ code/rtkCudaSplatImageFilter.cu
@@ -24,46 +24,8 @@
 
 // cuda includes
 #include <cuda.h>
-
-__global__
-void
-splat_kernel(float *input, int4 outputSize, float* output, int phase, float weight, unsigned int Blocks_Y)
-{
-  // CUDA 2.0 does not allow for a 3D grid, which severely
-  // limits the manipulation of large 3D arrays of data.  The
-  // following code is a hack to bypass this implementation
-  // limitation.
-  unsigned int blockIdx_z = blockIdx.y / Blocks_Y;
-  unsigned int blockIdx_y = blockIdx.y - __umul24(blockIdx_z, Blocks_Y);
-  unsigned int i = __umul24(blockIdx.x, blockDim.x) + threadIdx.x;
-  unsigned int j = __umul24(blockIdx_y, blockDim.y) + threadIdx.y;
-  unsigned int k = __umul24(blockIdx_z, blockDim.z) + threadIdx.z;
-
-  if (i >= outputSize.x || j >= outputSize.y || k >= outputSize.z || phase >= outputSize.w)
-      return;
-
-  long int output_idx = ((phase * outputSize.z + k) * outputSize.y + j) * outputSize.x + i;
-  long int input_idx = (k * outputSize.y + j) * outputSize.x + i;
-
-  output[output_idx] += input[input_idx] * weight;
-}
-
-__global__
-void
-splat_kernel_3Dgrid(float *input, int4 outputSize, float* output, int phase, float weight)
-{
-  unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
-  unsigned int j = blockIdx.y * blockDim.y + threadIdx.y;
-  unsigned int k = blockIdx.z * blockDim.z + threadIdx.z;
-
-  if (i >= outputSize.x || j >= outputSize.y || k >= outputSize.z || phase >= outputSize.w)
-      return;
-
-  long int output_idx = ((phase * outputSize.z + k) * outputSize.y + j) * outputSize.x + i;
-  long int input_idx = (k * outputSize.y + j) * outputSize.x + i;
-
-  output[output_idx] += input[input_idx] * weight;
-}
+#include <cublas_v2.h>
+#include <cuda_runtime.h>
 
 void
 CUDA_splat(const int4 &outputSize,
@@ -72,53 +34,27 @@ CUDA_splat(const int4 &outputSize,
                    int projectionNumber,
                    float **weights)
 {
+  cublasHandle_t  handle;
+  cublasCreate(&handle);
 
-  // Thread Block Dimensions
-  int tBlock_x = 16;
-  int tBlock_y = 4;
-  int tBlock_z = 4;
-  int  blocksInX = (outputSize.x - 1) / tBlock_x + 1;
-  int  blocksInY = (outputSize.y - 1) / tBlock_y + 1;
-  int  blocksInZ = (outputSize.z - 1) / tBlock_z + 1;
+  int numel = outputSize.x * outputSize.y * outputSize.z;
 
-  int device;
-  cudaGetDevice(&device);
-
-  if(CUDA_VERSION<4000 || GetCudaComputeCapability(device).first<=1)
+  for (int phase=0; phase<outputSize.w; phase++)
     {
-    dim3 dimGrid  = dim3(blocksInX, blocksInY*blocksInZ);
-    dim3 dimBlock = dim3(tBlock_x, tBlock_y, tBlock_z);
-
-    for (int phase=0; phase<outputSize.w; phase++)
+    float weight = weights[phase][projectionNumber];
+    if(weight!=0)
       {
-      float weight = weights[phase][projectionNumber];
-      if(weight!=0)
-        {
-          splat_kernel <<< dimGrid, dimBlock >>> ( input,
-                                                   outputSize,
-                                                   output,
-                                                   phase,
-                                                   weight,
-                                                   blocksInX);
-        }
+      // Create a pointer to the "phase"-th volume in the output
+      float * p = output + phase * numel;
+
+      // Add "weight" times the input to the "phase"-th volume in the output
+      cublasSaxpy(handle, numel, &weight, input, 1, p, 1);
       }
     }
-  else
-    {
-    dim3 dimGrid  = dim3(blocksInX, blocksInY, blocksInZ);
-    dim3 dimBlock = dim3(tBlock_x, tBlock_y, tBlock_z);
 
-    for (int phase=0; phase<outputSize.w; phase++)
-      {
-      float weight = weights[phase][projectionNumber];
-      if(weight!=0)
-        {
-            splat_kernel_3Dgrid <<< dimGrid, dimBlock >>> ( input,
-                                                     outputSize,
-                                                     output,
-                                                     phase,
-                                                     weight);
-        }
-      }
-    }
+  // Destroy Cublas context
+  cublasDestroy(handle);
+
+  CUDA_CHECK_ERROR;
+  cudaDeviceSynchronize();
 }
