515a7bdd22294144447b60d6463c1f57b29a7b74
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2016-01-22 07:37:00 +0100

    Added SimpleRTK (with and without CUDA) to nightly builds on CREATIS cluster

---
+++ linux15_fedora_gcc_nocuda_system_itk4_simplertk.cmake
@@ -0,0 +1,16 @@
+# client maintainer: simon.rit@creatis.insa-lyon.fr
+set(CTEST_SITE "linux15.dg.creatis.insa-lyon.fr")
+set(CTEST_BUILD_NAME "Linux-64bit-gcc-nocuda-system-itk4-simplertk")
+set(CTEST_BUILD_CONFIGURATION RelWithDebInfo)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_DASHBOARD_ROOT "/tmp/RTK_dashboard")
+set(dashboard_binary_name "RTK_lin64_gcc_nocuda_system_itk4_simplertk")
+set(ENV{ITK_DIR} "/home/srit/src/itk/lin64-dg")
+set(CTEST_BUILD_FLAGS -j8)
+
+SET(CONFIGURE_OPTIONS
+   -DRTK_USE_CUDA=FALSE
+   -DBUILD_SIMPLERTK:BOOL=ON)
+
+include("${CTEST_SCRIPT_DIRECTORY}/rtk_common.cmake")
+
