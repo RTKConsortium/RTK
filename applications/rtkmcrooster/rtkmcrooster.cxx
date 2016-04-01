1ce242683aa442d1c24cf616b1843cc9b8b53276
Cyril Mory <cyril.mory@uclouvain.be>
2016-04-01 09:29:15 +0200

    Added Motion-Compensated ROOSTER filter and the corresponding rtkmcrooster application

---
+++ applications/rtkmcrooster/rtkmcrooster.cxx
@@ -0,0 +1,224 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#include "rtkmcrooster_ggo.h"
+#include "rtkGgoFunctions.h"
+
+#include "rtkMotionCompensatedFourDROOSTERConeBeamReconstructionFilter.h"
+#include "rtkThreeDCircularProjectionGeometryXMLFile.h"
+#include "rtkPhasesToInterpolationWeights.h"
+#include "rtkWarpSequenceImageFilter.h"
+
+#ifdef RTK_USE_CUDA
+  #include "itkCudaImage.h"
+#endif
+#include <itkImageFileWriter.h>
+
+int main(int argc, char * argv[])
+{
+  GGO(rtkmcrooster, args_info);
+
+  typedef float OutputPixelType;
+  typedef itk::CovariantVector< OutputPixelType, 3 > DVFVectorType;
+
+#ifdef RTK_USE_CUDA
+  typedef itk::CudaImage< OutputPixelType, 4 >  VolumeSeriesType;
+  typedef itk::CudaImage< OutputPixelType, 3 >  ProjectionStackType;
+  typedef itk::CudaImage<DVFVectorType, VolumeSeriesType::ImageDimension> DVFSequenceImageType;
+  typedef itk::CudaImage<DVFVectorType, VolumeSeriesType::ImageDimension - 1> DVFImageType;
+#else
+  typedef itk::Image< OutputPixelType, 4 > VolumeSeriesType;
+  typedef itk::Image< OutputPixelType, 3 > ProjectionStackType;
+  typedef itk::Image<DVFVectorType, VolumeSeriesType::ImageDimension> DVFSequenceImageType;
+  typedef itk::Image<DVFVectorType, VolumeSeriesType::ImageDimension - 1> DVFImageType;
+#endif
+  typedef ProjectionStackType                   VolumeType;
+  typedef itk::ImageFileReader<  DVFSequenceImageType > DVFReaderType;
+
+  // Projections reader
+  typedef rtk::ProjectionsReader< ProjectionStackType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  rtk::SetProjectionsReaderFromGgo<ReaderType, args_info_rtkmcrooster>(reader, args_info);
+
+  // Geometry
+  if(args_info.verbose_flag)
+    std::cout << "Reading geometry information from "
+              << args_info.geometry_arg
+              << "..."
+              << std::endl;
+  rtk::ThreeDCircularProjectionGeometryXMLFileReader::Pointer geometryReader;
+  geometryReader = rtk::ThreeDCircularProjectionGeometryXMLFileReader::New();
+  geometryReader->SetFilename(args_info.geometry_arg);
+  TRY_AND_EXIT_ON_ITK_EXCEPTION( geometryReader->GenerateOutputInformation() )
+
+  // Create input: either an existing volume read from a file or a blank image
+  itk::ImageSource< VolumeSeriesType >::Pointer inputFilter;
+  if(args_info.input_given)
+    {
+    // Read an existing image to initialize the volume
+    typedef itk::ImageFileReader<  VolumeSeriesType > InputReaderType;
+    InputReaderType::Pointer inputReader = InputReaderType::New();
+    inputReader->SetFileName( args_info.input_arg );
+    inputFilter = inputReader;
+    }
+  else
+    {
+    // Create new empty volume
+    typedef rtk::ConstantImageSource< VolumeSeriesType > ConstantImageSourceType;
+    ConstantImageSourceType::Pointer constantImageSource = ConstantImageSourceType::New();
+    rtk::SetConstantImageSourceFromGgo<ConstantImageSourceType, args_info_rtkmcrooster>(constantImageSource, args_info);
+
+    // GenGetOpt can't handle default arguments for multiple arguments like dimension or spacing.
+    // The only default it accepts is to set all components of a multiple argument to the same value.
+    // Default dimension is 256^4, ie the number of reconstructed instants is 256. It has to be set to a more reasonable value
+    // which is why a "frames" argument is introduced
+    ConstantImageSourceType::SizeType inputSize = constantImageSource->GetSize();
+    inputSize[3] = args_info.frames_arg;
+    constantImageSource->SetSize(inputSize);
+
+    inputFilter = constantImageSource;
+    }
+  inputFilter->Update();
+  inputFilter->ReleaseDataFlagOn();
+
+  // Read the phases file
+  rtk::PhasesToInterpolationWeights::Pointer phaseReader = rtk::PhasesToInterpolationWeights::New();
+  phaseReader->SetFileName(args_info.signal_arg);
+  phaseReader->SetNumberOfReconstructedFrames(inputFilter->GetOutput()->GetLargestPossibleRegion().GetSize(3));
+  phaseReader->Update();
+  
+  // Create the 4DROOSTER filter, connect the basic inputs, and set the basic parameters
+  typedef rtk::MotionCompensatedFourDROOSTERConeBeamReconstructionFilter<VolumeSeriesType, ProjectionStackType, DVFSequenceImageType, DVFImageType> MCROOSTERFilterType;
+  MCROOSTERFilterType::Pointer mcrooster = MCROOSTERFilterType::New();
+  mcrooster->SetInputVolumeSeries(inputFilter->GetOutput() );
+  mcrooster->SetInputProjectionStack(reader->GetOutput());
+  mcrooster->SetGeometry( geometryReader->GetOutputObject() );
+  mcrooster->SetWeights(phaseReader->GetOutput());
+  mcrooster->SetCG_iterations( args_info.cgiter_arg );
+  mcrooster->SetMainLoop_iterations( args_info.niter_arg );
+  mcrooster->SetCudaConjugateGradient(args_info.cudacg_flag);
+  mcrooster->SetSignalFilename(args_info.signal_arg);
+  
+  // For each optional regularization step, set whether or not
+  // it should be performed, and provide the necessary inputs
+  
+  // Positivity
+  if (args_info.nopositivity_flag)
+    mcrooster->SetPerformPositivity(false);
+  else
+    mcrooster->SetPerformPositivity(true);
+  
+  // Motion mask
+  typedef itk::ImageFileReader<  VolumeType > InputReaderType;
+  if (args_info.motionmask_given)
+    {
+    InputReaderType::Pointer motionMaskReader = InputReaderType::New();
+    motionMaskReader->SetFileName( args_info.motionmask_arg );
+    motionMaskReader->Update();
+    mcrooster->SetMotionMask(motionMaskReader->GetOutput());
+    mcrooster->SetPerformMotionMask(true);
+    }
+  else
+    mcrooster->SetPerformMotionMask(false);
+    
+  // Spatial TV
+  if (args_info.gamma_space_given)
+    {
+    mcrooster->SetGammaTVSpace(args_info.gamma_space_arg);
+    mcrooster->SetTV_iterations(args_info.tviter_arg);
+    mcrooster->SetPerformTVSpatialDenoising(true);
+    }
+  else
+    mcrooster->SetPerformTVSpatialDenoising(false);
+  
+  // Spatial wavelets
+  if (args_info.threshold_given)
+    {
+    mcrooster->SetSoftThresholdWavelets(args_info.threshold_arg);
+    mcrooster->SetOrder(args_info.order_arg);
+    mcrooster->SetNumberOfLevels(args_info.levels_arg);
+    mcrooster->SetPerformWaveletsSpatialDenoising(true);
+    }
+  else
+    mcrooster->SetPerformWaveletsSpatialDenoising(false);
+  
+  // Temporal TV
+  if (args_info.gamma_time_given)
+    {
+    mcrooster->SetGammaTVTime(args_info.gamma_time_arg);
+    mcrooster->SetTV_iterations(args_info.tviter_arg);
+    mcrooster->SetPerformTVTemporalDenoising(true);
+    }
+  else
+    mcrooster->SetPerformTVTemporalDenoising(false);
+
+  // Temporal L0
+  if (args_info.lambda_time_arg)
+    {
+    mcrooster->SetLambdaL0Time(args_info.lambda_time_arg);
+    mcrooster->SetL0_iterations(args_info.l0iter_arg);
+    mcrooster->SetPerformL0TemporalDenoising(true);
+    }
+  else
+    mcrooster->SetPerformL0TemporalDenoising(false);
+
+  // Read DVF
+  DVFReaderType::Pointer dvfReader = DVFReaderType::New();
+  dvfReader->SetFileName( args_info.dvf_arg );
+  dvfReader->Update();
+  mcrooster->SetDisplacementField(dvfReader->GetOutput());
+
+  // Read inverse DVF if provided
+  DVFReaderType::Pointer idvfReader = DVFReaderType::New();
+  idvfReader->SetFileName( args_info.idvf_arg );
+  idvfReader->Update();
+  mcrooster->SetInverseDisplacementField(idvfReader->GetOutput());
+
+  itk::TimeProbe readerProbe;
+  if(args_info.time_flag)
+    {
+    std::cout << "Recording elapsed time... " << std::flush;
+    readerProbe.Start();
+    }
+
+  TRY_AND_EXIT_ON_ITK_EXCEPTION( mcrooster->Update() );
+
+  if(args_info.time_flag)
+    {
+    mcrooster->PrintTiming(std::cout);
+    readerProbe.Stop();
+    std::cout << "It took...  " << readerProbe.GetMean() << ' ' << readerProbe.GetUnit() << std::endl;
+    }
+
+  // MCROOSTER outputs a motion-compensated reconstruction
+  // Warp it with the inverse field so that it is 4D
+  typedef rtk::WarpSequenceImageFilter<VolumeSeriesType, DVFSequenceImageType, ProjectionStackType, DVFImageType> WarpSequenceFilterType;
+  WarpSequenceFilterType::Pointer warp = WarpSequenceFilterType::New();
+  warp->SetInput(mcrooster->GetOutput());
+  warp->SetDisplacementField(idvfReader->GetOutput());
+  TRY_AND_EXIT_ON_ITK_EXCEPTION( warp->Update() );
+
+  // Write
+  typedef itk::ImageFileWriter< VolumeSeriesType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( args_info.output_arg );
+  writer->SetInput( warp->GetOutput() );
+  TRY_AND_EXIT_ON_ITK_EXCEPTION( writer->Update() );
+
+  return EXIT_SUCCESS;
+}
---
+++ code/rtkMotionCompensatedFourDROOSTERConeBeamReconstructionFilter.hxx
@@ -0,0 +1,92 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+#ifndef __rtkMotionCompensatedFourDROOSTERConeBeamReconstructionFilter_hxx
+#define __rtkMotionCompensatedFourDROOSTERConeBeamReconstructionFilter_hxx
+
+#include "rtkMotionCompensatedFourDROOSTERConeBeamReconstructionFilter.h"
+#include <itkImageFileWriter.h>
+
+namespace rtk
+{
+
+template<typename VolumeSeriesType, typename ProjectionStackType, typename TMVFImageSequence, typename TMVFImage>
+MotionCompensatedFourDROOSTERConeBeamReconstructionFilter< VolumeSeriesType, ProjectionStackType, TMVFImageSequence, TMVFImage>
+::MotionCompensatedFourDROOSTERConeBeamReconstructionFilter()
+{
+  // Create the filters
+#ifdef RTK_USE_CUDA
+  this->m_FourDCGFilter = MotionCompensatedFourDCGFilterType::New();
+#endif
+}
+
+template<typename VolumeSeriesType, typename ProjectionStackType, typename TMVFImageSequence, typename TMVFImage>
+void
+MotionCompensatedFourDROOSTERConeBeamReconstructionFilter< VolumeSeriesType, ProjectionStackType, TMVFImageSequence, TMVFImage>
+::SetSignalFilename(const std::string _arg)
+{
+  dynamic_cast<MotionCompensatedFourDCGFilterType*>(this->m_FourDCGFilter.GetPointer())->SetSignalFilename(_arg);
+}
+
+template<typename VolumeSeriesType, typename ProjectionStackType, typename TMVFImageSequence, typename TMVFImage>
+void
+MotionCompensatedFourDROOSTERConeBeamReconstructionFilter< VolumeSeriesType, ProjectionStackType, TMVFImageSequence, TMVFImage>
+::GenerateInputRequestedRegion()
+{
+  // Set variables so that the superclass' implementation
+  // generates the correct input requested regions
+  this->m_PerformWarping = true;
+  this->m_ComputeInverseWarpingByConjugateGradient = false;
+
+  //Call the superclass' implementation of this method
+  Superclass::GenerateInputRequestedRegion();
+}
+
+template<typename VolumeSeriesType, typename ProjectionStackType, typename TMVFImageSequence, typename TMVFImage>
+void
+MotionCompensatedFourDROOSTERConeBeamReconstructionFilter< VolumeSeriesType, ProjectionStackType, TMVFImageSequence, TMVFImage>
+::GenerateOutputInformation()
+{
+  // Set m_PerformWarping to false so as not
+  // to plug any of the warping filters into the pipeline
+  // (DVFs are taken into account in the motion compensated 4D conjugate gradient filter)
+  this->m_PerformWarping = false;
+
+  // Set the 4D conjugate gradient filter
+  dynamic_cast<MotionCompensatedFourDCGFilterType*>(this->m_FourDCGFilter.GetPointer())->SetDisplacementField(this->GetDisplacementField());
+  dynamic_cast<MotionCompensatedFourDCGFilterType*>(this->m_FourDCGFilter.GetPointer())->SetInverseDisplacementField(this->GetInverseDisplacementField());
+
+  // Call the superclass implementation
+  Superclass::GenerateOutputInformation();
+}
+
+template<typename VolumeSeriesType, typename ProjectionStackType, typename TMVFImageSequence, typename TMVFImage>
+void
+MotionCompensatedFourDROOSTERConeBeamReconstructionFilter< VolumeSeriesType, ProjectionStackType, TMVFImageSequence, TMVFImage>
+::GenerateData()
+{
+  // Set the variables so that the superclass implementation updates the right filters
+  this->m_PerformWarping = false;
+
+  // Call the superclass implementation
+  Superclass::GenerateData();
+}
+
+}// end namespace
+
+
+#endif
