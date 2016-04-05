f56d9f2cf81d16203ffc0302cda61f14c7cb250c
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2016-04-05 18:16:12 +0200

    Added accessor to projection matrix

--- utilities/SimpleRTK/Code/Common/include/srtkThreeDCircularProjectionGeometry.h
+++ utilities/SimpleRTK/Code/Common/include/srtkThreeDCircularProjectionGeometry.h
@@ -76,6 +76,7 @@ public:
   const std::vector<double> &GetProjectionOffsetsY() const;
   const std::vector<double>  GetSourcePosition( const unsigned int i ) const;
   const std::vector<double>  GetRotationMatrix( const unsigned int i) const;
+  const std::vector<double>  GetMatrix( const unsigned int i) const;
 
   /** Add the projection with angles in degress
    * @{
