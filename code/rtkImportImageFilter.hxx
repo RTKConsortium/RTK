710e009e581a6e1edd40d2c703fc9220b691a3a3
Cyril Mory <cyril.mory@gmail.com>
2017-06-12 15:07:56 +0200

    Fixed bug in rtkImportImageFilter: the CudaImageDataManager did not correctly allocate the GPU buffer

--- code/rtkImportImageFilter.hxx
+++ code/rtkImportImageFilter.hxx
@@ -197,6 +197,19 @@ ImportImageFilter< TImage >
   // opposed to the container) if the user wants it to.
   outputPtr->GetPixelContainer()->SetImportPointer(m_ImportPointer,
                                                    m_Size, false);
+
+#ifdef RTK_USE_CUDA
+  typedef itk::CudaImage<typename TImage::PixelType, TImage::ImageDimension> TCudaImage;
+  if (TCudaImage* cudaOutputPtr = dynamic_cast<TCudaImage*>(outputPtr.GetPointer()))
+    {
+    cudaOutputPtr->GetDataManager()->SetBufferSize(m_Size * sizeof(typename OutputImageType::PixelType));
+    cudaOutputPtr->GetDataManager()->SetImagePointer(outputPtr);
+    cudaOutputPtr->GetDataManager()->SetCPUBufferPointer(m_ImportPointer);
+    cudaOutputPtr->GetDataManager()->SetGPUDirtyFlag(true);
+    cudaOutputPtr->GetDataManager()->SetCPUDirtyFlag(false);
+    cudaOutputPtr->GetDataManager()->SetTimeStamp(outputPtr->GetPixelContainer()->GetTimeStamp());
+    }
+#endif
 }
 
 //----------------------------------------------------------------------------
