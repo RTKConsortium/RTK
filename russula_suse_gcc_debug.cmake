ffa0d13b283420192ef2467230c5892670d789e8
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2017-08-30 11:01:37 +0200

    Added CUDA path to russula compilations

--- russula_suse_gcc_debug.cmake
+++ russula_suse_gcc_debug.cmake
@@ -6,6 +6,7 @@ set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(dashboard_binary_name "RTK_lin64_gcc_debug")
 set(ENV{ITK_DIR} "/home/srit/src/itk/lin64")
 set(CTEST_BUILD_FLAGS -j16)
+set(ENV{CUDA_BIN_PATH} "/home/srit/Download/cuda80/bin")
 
 set(CONFIGURE_OPTIONS
   -DCUDA_NVCC_FLAGS:STRING=-std=c++11
