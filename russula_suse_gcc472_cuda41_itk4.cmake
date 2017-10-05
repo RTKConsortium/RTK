462d590d87310a6b023e9332ed3b1402c3a0c726
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2017-10-05 07:45:48 +0200

    Test CUDA 4.1, CUDA 4.0 is no longer supported

--- russula_suse_gcc472_cuda41_itk4.cmake
+++ russula_suse_gcc472_cuda41_itk4.cmake
@@ -1,16 +1,16 @@
 # client maintainer: simon.rit@creatis.insa-lyon.fr
 set(CTEST_SITE "russula.clb")
-set(CTEST_BUILD_NAME "Linux-64bit-gcc472-cuda40-itk4")
+set(CTEST_BUILD_NAME "Linux-64bit-gcc472-cuda41-itk4")
 set(CTEST_BUILD_CONFIGURATION Release)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(dashboard_binary_name "RTK_lin64_gcc472_cuda40_itk4")
+set(dashboard_binary_name "RTK_lin64_gcc472_cuda41_itk4")
 set(ENV{ITK_DIR} "/home/srit/src/itk/lin64_gcc_472")
-set(ENV{CUDA_BIN_PATH} "/home/srit/Download/cuda40/bin")
+set(ENV{CUDA_BIN_PATH} "/home/srit/Download/cuda41/bin")
 set(CTEST_BUILD_FLAGS -j12)
 
 set(ENV{CC} "/home/srit/src/gcc/gcc472-install/bin/gcc")
 set(ENV{CXX} "/home/srit/src/gcc/gcc472-install/bin/c++")
-set(ENV{PATH} "/home/srit/src/gcc/gcc440-install/bin:$ENV{PATH}")
+set(ENV{PATH} "/home/srit/src/gcc/gcc441-install/bin:$ENV{PATH}")
 set(ENV{CXXFLAGS} "")
 
 include("${CTEST_SCRIPT_DIRECTORY}/rtk_common.cmake")
