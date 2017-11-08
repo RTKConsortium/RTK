26d7cd898c5263374333f657aa1028ac989ade59
Lucas Gandel <lucas.gandel@kitware.com>
2017-11-08 10:52:04 +0100

    Add support for building RTK within ITK

--- utilities/ITKCudaCommon/include/itkCudaWin32Header.h
+++ utilities/ITKCudaCommon/include/itkCudaWin32Header.h
@@ -19,9 +19,8 @@
 #define itkCudaWin32Header_h
 
 #include "itkConfigure.h"
-#include "rtkConfiguration.h"
 
-#if (defined(_WIN32) || defined(WIN32)) && defined(RTK_BUILD_SHARED_LIBS) 
+#if (defined(_WIN32) || defined(WIN32)) && defined(ITK_BUILD_SHARED_LIBS)
 # ifdef ITKCudaCommon_EXPORTS
 #  define ITKCudaCommon_EXPORT __declspec(dllexport)
 # else
--- utilities/ITKCudaCommon/itk-module.cmake
+++ utilities/ITKCudaCommon/itk-module.cmake
@@ -1,22 +1,12 @@
-if (ITK_USE_CUDA)
 
-  get_filename_component(MY_CURENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-  file(READ "${MY_CURENT_DIR}/README" DOCUMENTATION)
+get_filename_component(_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+file(READ "${_CURRENT_DIR}/README" DOCUMENTATION)
 
-  option(Module_ITKCudaCommon "Compile the ITK Cuda external module" FALSE)
-
-  if (${Module_ITKCudaCommon})
-
-    # define the dependencies of the include module and the tests
-    itk_module(ITKCudaCommon
-      DEPENDS
-        ITKCommon
-      TEST_DEPENDS
-        ITKTestKernel
-      DESCRIPTION
-        "${DOCUMENTATION}"
-    )
-
-  endif()
-
-endif()
+itk_module(ITKCudaCommon
+  DEPENDS
+    ITKCommon
+  TEST_DEPENDS
+    ITKTestKernel
+  DESCRIPTION
+    "${DOCUMENTATION}"
+  )
