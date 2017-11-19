53a74196d7e651dd60b91a1072e140364b9b2e2d
LucasGandel <lucas.gandel@gmail.com>
2017-11-19 18:09:22 +0100

    Generate ITKCudaCommon target within RTK
    When building RTK within ITK, the conditionnal dependency on
    Cuda can not be resolved during ITK's first pass in itk-module.cmake.
    It results in ITKCudaCommon not being build except it is copied
    into the External directory.
    Untill ITKCudaCommon becomes an external module, we explicitely link
    against ITKCudaCommon library and include ITKCudaCommon directories.

--- utilities/ITKCudaCommon/itk-module.cmake
+++ utilities/ITKCudaCommon/itk-module.cmake
@@ -1,12 +1,15 @@
+if(ITK_SOURCE_DIR)
+  get_filename_component(_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+  file(READ "${_CURRENT_DIR}/README" DOCUMENTATION)
 
-get_filename_component(_CURRENT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-file(READ "${_CURRENT_DIR}/README" DOCUMENTATION)
-
-itk_module(ITKCudaCommon
-  DEPENDS
-    ITKCommon
-  TEST_DEPENDS
-    ITKTestKernel
-  DESCRIPTION
-    "${DOCUMENTATION}"
-  )
+  itk_module(ITKCudaCommon
+    ENABLE_SHARED
+    EXCLUDE_FROM_DEFAULT
+    DEPENDS
+      ITKCommon
+    TEST_DEPENDS
+      ITKTestKernel
+    DESCRIPTION
+      "${DOCUMENTATION}"
+    )
+endif()
