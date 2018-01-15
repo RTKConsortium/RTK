bb06e11bebad534719ff1c7e590ee4f0e723d5f1
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2018-01-15 16:30:57 +0100

    ENH: Implementation of bioscan NanoSPECT/CT data

---
+++ testing/rtkbioscantest.cxx
@@ -0,0 +1,44 @@
+#include "rtkTest.h"
+#include "rtkMacro.h"
+#include "rtkBioscanGeometryReader.h"
+#include "rtkThreeDCircularProjectionGeometryXMLFile.h"
+
+#include <itkRegularExpressionSeriesFileNames.h>
+
+/**
+ * \file rtkbioscantest.cxx
+ *
+ * \brief Functional tests for classes managing Bioscan NanoSPECT/CT data
+ *
+ * This test reads and verifies the geometry from a bioscan projection.
+ *
+ * \author Simon Rit
+ */
+
+int main(int, char** )
+{
+  std::cout << "Testing geometry..." << std::endl;
+
+  // Ora geometry
+  std::vector<std::string> filenames;
+  filenames.push_back( std::string(RTK_DATA_ROOT) +
+                       std::string("/Input/Bioscan/Bioscan.dcm") );
+  rtk::BioscanGeometryReader::Pointer geoTargReader;
+  geoTargReader = rtk::BioscanGeometryReader::New();
+  geoTargReader->SetProjectionsFileNames( filenames );
+  TRY_AND_EXIT_ON_ITK_EXCEPTION( geoTargReader->UpdateOutputData() );
+
+  // Reference geometry
+  rtk::ThreeDCircularProjectionGeometryXMLFileReader::Pointer geoRefReader;
+  geoRefReader = rtk::ThreeDCircularProjectionGeometryXMLFileReader::New();
+  geoRefReader->SetFilename( std::string(RTK_DATA_ROOT) +
+                             std::string("/Baseline/Bioscan/geometry.xml") );
+  TRY_AND_EXIT_ON_ITK_EXCEPTION( geoRefReader->GenerateOutputInformation() )
+
+  // Check geometries
+  CheckGeometries(geoTargReader->GetGeometry(), geoRefReader->GetOutputObject() );
+
+  // If all succeed
+  std::cout << "\n\nTest PASSED! " << std::endl;
+  return EXIT_SUCCESS;
+}
