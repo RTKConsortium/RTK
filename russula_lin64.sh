11036fd8213028eb6360accb9a18cebcf6c9c476
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2018-04-04 21:13:33 +0200

    Remove gcc472 from russula nighly builds, incompatible with ITK 5

--- russula_lin64.sh
+++ russula_lin64.sh
@@ -7,6 +7,4 @@ ctest -S /home/srit/src/rtk/rtk-dashboard/russula_doxygen.cmake -V
 rsync -e 'ssh -i /home/srit/.ssh/nophrase' -a --delete \
     /home/srit/src/rtk/dashboard_tests/RTK-Doxygen/Doxygen/html \
     ssh.creatis.insa-lyon.fr:/home/srit/src/rtk/dashboard_tests/RTK-Doxygen/Doxygen
-export PATH=/home/srit/src/cmake/lin64_gcc_472/bin:$PATH
-ctest -S /home/srit/src/rtk/rtk-dashboard/russula_suse_gcc472_cuda41_itk4.cmake -V
 
