9a9508cdfc1898552a9711eb91dbe1c42f33f270
cyril <cyril.mory@gmail.com>
2018-04-23 15:51:05 +0200

    Shiitake (Windows) CI

--- shiitake.bat
+++ shiitake.bat
@@ -1,9 +1,2 @@
-ctest -VV -S shiitake_static_release.cmake
-ctest -VV -S shiitake_shared_release.cmake
-ctest -VV -S shiitake_static_release_simplertk.cmake
-ctest -VV -S shiitake_shared_release_simplertk.cmake
-ctest -VV -S shiitake_static_release_nofftw.cmake
-ctest -VV -S shiitake_static_debug_nofftw.cmake
-ctest -VV -S shiitake_static_debug.cmake
-ctest -VV -S shiitake_shared_debug.cmake
+ctest -VV -S shiitake.cmake
 REM C:\Windows\System32\shutdown.exe /s
