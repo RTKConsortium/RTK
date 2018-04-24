5c2ebd9758adf47573bb47e6e89c4597dde41ed6
Cyril Mory <cyril.mory@gmail.com>
2018-04-24 10:50:11 +0200

    ENH: Added Mechlem filter

---
+++ include/rtkMechlemOneStepSpectralReconstructionFilter.h
@@ -0,0 +1,219 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef rtkMechlemOneStepSpectralReconstructionFilter_h
+#define rtkMechlemOneStepSpectralReconstructionFilter_h
+
+#include "rtkIterativeConeBeamReconstructionFilter.h"
+#include "rtkThreeDCircularProjectionGeometry.h"
+#include "rtkWeidingerForwardModelImageFilter.h"
+#include "rtkGetNewtonUpdateImageFilter.h"
+#include "rtkConstantImageSource.h"
+#include <itkSubtractImageFilter.h>
+
+namespace rtk
+{
+  /** \class MechlemOneStepSpectralReconstructionFilter
+   * \brief Implements the one-step spectral CT inversion method described by Mechlem et al.
+   *
+   * This filter implements one-step spectral CT inversion method described by Mechlem et al.
+   * in their paper "Joint statistical iterative material image reconstruction
+   * for spectral computed tomography using a semi-empirical forward model", IEEE TMI 2017
+   * It reconstructs a vector-valued volume (each component is a material) from vector-valued
+   * projections (each component is the count of photons in an energy bin of the spectral detector).
+   * It requires knowledge of the incident spectrum, of the detector's energy distribution and
+   * of the materials' matrix of mass-attenuation coefficients as a function of the incident energy.
+   *
+   * \dot
+   * digraph MechlemOneStepSpectralReconstructionFilter {
+   * 
+   * Input0 [ label="Input 0 (Material volumes)"];
+   * Input0 [shape=Mdiamond];
+   * Input1 [label="Input 1 (Photon counts)"];
+   * Input1 [shape=Mdiamond];
+   * Input2 [label="Input 2 (Incident spectrum)"];
+   * Input2 [shape=Mdiamond];
+   * Output [label="Output (Material volumes)"];
+   * Output [shape=Mdiamond];
+   * 
+   * node [shape=box];
+   * VolumeSource [ label="rtk::ConstantImageSource (1 component volume, full of ones)" URL="\ref rtk::ConstantImageSource"];
+   * SingleComponentProjectionsSource [ label="rtk::ConstantImageSource (1 component projections, full of zeros)" URL="\ref rtk::ConstantImageSource"];
+   * VolumeSourceGradients [ label="rtk::ConstantImageSource (m components)" URL="\ref rtk::ConstantImageSource"];
+   * VolumeSourceHessians [ label="rtk::ConstantImageSource (m x m components)" URL="\ref rtk::ConstantImageSource"];
+   * ProjectionsSource [ label="rtk::ConstantImageSource (m components)" URL="\ref rtk::ConstantImageSource"];
+   * ForwardProjection [ label="rtk::ForwardProjectionImageFilter" URL="\ref rtk::ForwardProjectionImageFilter"];
+   * SingleComponentForwardProjection [ label="rtk::ForwardProjectionImageFilter (1 component)" URL="\ref rtk::ForwardProjectionImageFilter"];
+   * BackProjectionGradients [ label="rtk::BackProjectionImageFilter (gradients)" URL="\ref rtk::BackProjectionImageFilter"];
+   * BackProjectionHessians [ label="rtk::BackProjectionImageFilter (hessians)" URL="\ref rtk::BackProjectionImageFilter"];
+   * Weidinger[ label="rtk::WeidingerForwardModelImageFilter" URL="\ref rtk::WeidingerForwardModelImageFilter"];
+   * Newton[ label="rtk::GetNewtonUpdateImageFilter" URL="\ref rtk::GetNewtonUpdateImageFilter"];
+   * Subtract[ label="itk::SubtractImageFilter" URL="\ref itk::SubtractImageFilter"];
+   * AfterInput0 [label="", fixedsize="false", width=0, height=0, shape=none];
+   * AfterSubtract [label="", fixedsize="false", width=0, height=0, shape=none];
+   *
+   * Input0 -> AfterInput0 [arrowhead=none];
+   * AfterInput0 -> ForwardProjection;
+   * ProjectionsSource -> ForwardProjection;
+   * Input1 -> Weidinger;
+   * Input2 -> Weidinger;
+   * ForwardProjection -> Weidinger;
+   * VolumeSourceGradients -> BackProjectionGradients;
+   * VolumeSourceHessians -> BackProjectionHessians;
+   * VolumeSource -> SingleComponentForwardProjection;
+   * SingleComponentProjectionsSource -> SingleComponentForwardProjection;
+   * SingleComponentForwardProjection -> Weidinger;
+   * Weidinger -> BackProjectionGradients;
+   * Weidinger -> BackProjectionHessians;
+   * BackProjectionGradients -> Newton;
+   * BackProjectionHessians -> Newton;
+   * Newton -> Subtract;
+   * AfterInput0 -> Subtract;
+   * Subtract -> AfterSubtract [arrowhead=none];
+   * AfterSubtract -> Output;
+   * AfterSubtract -> AfterInput0 [style=dashed, constraint=false]
+   * }
+   * \enddot
+   *
+   * \test rtkmechlemtest.cxx
+   *
+   * \author Cyril Mory
+   *
+   * \ingroup ReconstructionAlgorithm
+   */
+
+template< typename TOutputImage, typename TPhotonCounts, typename TSpectrum >
+class MechlemOneStepSpectralReconstructionFilter : public rtk::IterativeConeBeamReconstructionFilter<TOutputImage, TOutputImage>
+{
+public:
+    /** Standard class typedefs. */
+    typedef MechlemOneStepSpectralReconstructionFilter                      Self;
+    typedef IterativeConeBeamReconstructionFilter<TOutputImage, TOutputImage>  Superclass;
+    typedef itk::SmartPointer< Self >                                          Pointer;
+
+    /** Method for creation through the object factory. */
+    itkNewMacro(Self)
+
+    /** Run-time type information (and related methods). */
+    itkTypeMacro(MechlemOneStepSpectralReconstructionFilter, itk::ImageToImageFilter)
+
+    /** Internal typedef */
+    typedef itk::Image< itk::Vector<typename TOutputImage::PixelValueType, TOutputImage::PixelType::Dimension * TOutputImage::PixelType::Dimension> > THessiansImage;
+    typedef TOutputImage TGradientsImage;
+    typedef itk::Image<typename TOutputImage::PixelValueType, TOutputImage::ImageDimension> TSingleComponentImage;
+
+    /** Filter typedefs */
+    typedef rtk::ForwardProjectionImageFilter< TSingleComponentImage, TSingleComponentImage > SingleComponentForwardProjectionFilterType;
+    typedef rtk::ForwardProjectionImageFilter< TOutputImage, TOutputImage >               ForwardProjectionFilterType;
+    typedef rtk::BackProjectionImageFilter< TGradientsImage, TGradientsImage >            GradientsBackProjectionFilterType;
+    typedef rtk::BackProjectionImageFilter< THessiansImage, THessiansImage >              HessiansBackProjectionFilterType;
+    typedef itk::SubtractImageFilter<TOutputImage>                                        SubtractFilterType;
+    typedef rtk::ConstantImageSource<TSingleComponentImage>                                 SingleComponentImageSourceType;
+    typedef rtk::ConstantImageSource<TOutputImage>                                        MaterialProjectionsSourceType;
+    typedef rtk::ConstantImageSource<TGradientsImage>                                     GradientsSourceType;
+    typedef rtk::ConstantImageSource<THessiansImage>                                      HessiansSourceType;
+    typedef rtk::WeidingerForwardModelImageFilter<TOutputImage, TPhotonCounts, TSpectrum> WeidingerForwardModelType;
+    typedef rtk::GetNewtonUpdateImageFilter<TGradientsImage, THessiansImage>              NewtonFilterType;
+
+    /** Pass the ForwardProjection filter to the conjugate gradient operator */
+    void SetForwardProjectionFilter (int _arg) ITK_OVERRIDE;
+
+    /** Pass the backprojection filter to the conjugate gradient operator and to the back projection filter generating the B of AX=B */
+    void SetBackProjectionFilter (int _arg) ITK_OVERRIDE;
+
+    /** Pass the geometry to all filters needing it */
+    itkSetObjectMacro(Geometry, ThreeDCircularProjectionGeometry)
+
+    itkSetMacro(NumberOfIterations, int)
+    itkGetMacro(NumberOfIterations, int)
+
+    /** Set methods for all inputs, since they have different types */
+    void SetInputMaterialVolumes(const TOutputImage* materialVolumes);
+    void SetInputPhotonCounts(const TPhotonCounts* photonCounts);
+    void SetInputSpectrum(const TSpectrum* spectrum);
+
+//    itkSetMacro(IterationCosts, bool)
+//    itkGetMacro(IterationCosts, bool)
+
+//    /** If Regularized, perform laplacian-based regularization during
+//     *  reconstruction (gamma is the strength of the regularization) */
+//    itkSetMacro(Regularized, bool)
+//    itkGetMacro(Regularized, bool)
+//    itkSetMacro(Gamma, float)
+//    itkGetMacro(Gamma, float)
+
+protected:
+    MechlemOneStepSpectralReconstructionFilter();
+    virtual ~MechlemOneStepSpectralReconstructionFilter() ITK_OVERRIDE {}
+
+    /** Does the real work. */
+    void GenerateData() ITK_OVERRIDE;
+
+    /** Member pointers to the filters used internally (for convenience)*/
+    typename SingleComponentForwardProjectionFilterType::Pointer             m_SingleComponentForwardProjectionFilter;
+    typename MaterialProjectionsSourceType::Pointer                          m_ProjectionsSource;
+    typename SingleComponentImageSourceType::Pointer                         m_SingleComponentProjectionsSource;
+    typename SingleComponentImageSourceType::Pointer                         m_SingleComponentVolumeSource;
+    typename GradientsSourceType::Pointer                                    m_GradientsSource;
+    typename HessiansSourceType::Pointer                                     m_HessiansSource;
+    typename WeidingerForwardModelType::Pointer                              m_WeidingerForward;
+    typename NewtonFilterType::Pointer                                       m_NewtonFilter;
+    typename SubtractFilterType::Pointer                                     m_SubtractFilter;
+    typename ForwardProjectionFilterType::Pointer                            m_ForwardProjectionFilter;
+    typename GradientsBackProjectionFilterType::Pointer                      m_GradientsBackProjectionFilter;
+    typename HessiansBackProjectionFilterType::Pointer                       m_HessiansBackProjectionFilter;
+
+    /** The inputs of this filter have the same type but not the same meaning
+    * It is normal that they do not occupy the same physical space. Therefore this check
+    * must be removed */
+    void VerifyInputInformation() ITK_OVERRIDE {}
+
+    /** The volume and the projections must have different requested regions
+    */
+    void GenerateInputRequestedRegion() ITK_OVERRIDE;
+    void GenerateOutputInformation() ITK_OVERRIDE;
+
+    /** Getters for the inputs */
+    typename TOutputImage::ConstPointer GetInputMaterialVolumes();
+    typename TPhotonCounts::ConstPointer GetInputPhotonCounts();
+    typename TSpectrum::ConstPointer GetInputSpectrum();
+
+    /** Functions to instantiate forward and back projection filters with a different
+     * number of components than the ones provided by the IterativeConeBeamReconstructionFilter class */
+    typename SingleComponentForwardProjectionFilterType::Pointer InstantiateSingleComponentForwardProjectionFilter(int fwtype);
+    typename HessiansBackProjectionFilterType::Pointer InstantiateHessiansBackProjectionFilter (int bptype);
+
+private:
+    MechlemOneStepSpectralReconstructionFilter(const Self &); //purposely not implemented
+    void operator=(const Self &);  //purposely not implemented
+
+    ThreeDCircularProjectionGeometry::Pointer m_Geometry;
+
+    int                          m_NumberOfIterations;
+//    float                        m_Gamma;
+//    bool                         m_IterationCosts;
+//    bool                         m_Regularized;
+};
+} //namespace ITK
+
+
+#ifndef ITK_MANUAL_INSTANTIATION
+#include "rtkMechlemOneStepSpectralReconstructionFilter.hxx"
+#endif
+
+#endif
---
+++ include/rtkMechlemOneStepSpectralReconstructionFilter.hxx
@@ -0,0 +1,312 @@
+/*=========================================================================
+ *
+ *  Copyright RTK Consortium
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *=========================================================================*/
+
+#ifndef rtkMechlemOneStepSpectralReconstructionFilter_hxx
+#define rtkMechlemOneStepSpectralReconstructionFilter_hxx
+
+#include "rtkMechlemOneStepSpectralReconstructionFilter.h"
+
+namespace rtk
+{
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>::MechlemOneStepSpectralReconstructionFilter()
+{
+  this->SetNumberOfRequiredInputs(3);
+
+  // Set the default values of member parameters
+  m_NumberOfIterations=3;
+//  m_IterationCosts=false;
+//  m_Gamma = 0;
+//  m_Regularized = false;
+
+  // Create the filters
+  m_ProjectionsSource = MaterialProjectionsSourceType::New();
+  m_SingleComponentProjectionsSource = SingleComponentImageSourceType::New();
+  m_SingleComponentVolumeSource = SingleComponentImageSourceType::New();
+  m_GradientsSource = GradientsSourceType::New();
+  m_HessiansSource = HessiansSourceType::New();
+  m_WeidingerForward = WeidingerForwardModelType::New();
+  m_NewtonFilter = NewtonFilterType::New();
+  m_SubtractFilter = SubtractFilterType::New();
+
+  // Set permanent parameters
+  m_ProjectionsSource->SetConstant(itk::NumericTraits<typename TOutputImage::PixelType>::Zero);
+  m_SingleComponentProjectionsSource->SetConstant(itk::NumericTraits<typename TOutputImage::PixelValueType>::Zero);
+  m_SingleComponentVolumeSource->SetConstant(itk::NumericTraits<typename TOutputImage::PixelValueType>::One);
+  m_GradientsSource->SetConstant(itk::NumericTraits<typename MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>::TGradientsImage::PixelType>::Zero);
+  m_HessiansSource->SetConstant(itk::NumericTraits<typename MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>::THessiansImage::PixelType>::Zero);
+
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+void
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::SetInputMaterialVolumes(const TOutputImage* materialVolumes)
+{
+  this->SetNthInput(0, const_cast<TOutputImage*>(materialVolumes));
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+void
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::SetInputPhotonCounts(const TPhotonCounts* photonCounts)
+{
+  this->SetNthInput(1, const_cast<TPhotonCounts*>(photonCounts));
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+void
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::SetInputSpectrum(const TSpectrum* spectrum)
+{
+  this->SetNthInput(2, const_cast<TSpectrum*>(spectrum));
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+typename TOutputImage::ConstPointer
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::GetInputMaterialVolumes()
+{
+  return static_cast< const TOutputImage * >
+         ( this->itk::ProcessObject::GetInput(0) );
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+typename TPhotonCounts::ConstPointer
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::GetInputPhotonCounts()
+{
+  return static_cast< const TPhotonCounts * >
+         ( this->itk::ProcessObject::GetInput(1) );
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+typename TSpectrum::ConstPointer
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::GetInputSpectrum()
+{
+  return static_cast< const TSpectrum * >
+         ( this->itk::ProcessObject::GetInput(2) );
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+void
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::SetForwardProjectionFilter (int _arg)
+{
+  if( _arg != this->GetForwardProjectionFilter() )
+    {
+    Superclass::SetForwardProjectionFilter( _arg );
+    m_ForwardProjectionFilter = this->InstantiateForwardProjectionFilter( _arg ); // The multi-component one
+    m_SingleComponentForwardProjectionFilter = InstantiateSingleComponentForwardProjectionFilter( _arg ); // The single-component one
+    }
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+void
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::SetBackProjectionFilter (int _arg)
+{
+  if( _arg != this->GetBackProjectionFilter() )
+    {
+    Superclass::SetBackProjectionFilter( _arg );
+    m_GradientsBackProjectionFilter = this->InstantiateBackProjectionFilter( _arg );
+    m_HessiansBackProjectionFilter = this->InstantiateHessiansBackProjectionFilter( _arg );
+    }
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+typename MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>::SingleComponentForwardProjectionFilterType::Pointer
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::InstantiateSingleComponentForwardProjectionFilter (int fwtype)
+{
+  // Define the type of image to be back projected
+  typedef typename MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>::TSingleComponentImage TSingleComponent;
+
+  // Declare the pointer
+  typename MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>::SingleComponentForwardProjectionFilterType::Pointer fw;
+  switch(fwtype)
+    {
+    case(0):
+      fw = rtk::JosephForwardProjectionImageFilter<TSingleComponent, TSingleComponent>::New();
+    break;
+    case(1):
+    #ifdef RTK_USE_CUDA
+      fw = rtk::CudaForwardProjectionImageFilter<TSingleComponent, TSingleComponent>::New();
+    #else
+      itkGenericExceptionMacro(<< "The program has not been compiled with cuda option");
+    #endif
+    break;
+
+    default:
+      itkGenericExceptionMacro(<< "Unhandled --fp value.");
+    }
+  return fw;
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+typename MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>::HessiansBackProjectionFilterType::Pointer
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::InstantiateHessiansBackProjectionFilter (int bptype)
+{
+  // Define the type of image to be back projected
+  typedef typename MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>::THessiansImage THessians;
+
+  // Declare the pointer
+  typename MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>::HessiansBackProjectionFilterType::Pointer bp;
+  switch(bptype)
+    {
+    case(0):
+      bp = rtk::BackProjectionImageFilter<THessians,THessians>::New();
+      break;
+    case(1):
+      bp = rtk::JosephBackProjectionImageFilter<THessians,THessians>::New();
+      break;
+    case(2):
+    #ifdef RTK_USE_CUDA
+      // The current CUDA implementation only allows one component
+//      bp = rtk::CudaBackProjectionImageFilter::New();
+    #else
+      itkGenericExceptionMacro(<< "The program has not been compiled with cuda option");
+    #endif
+    break;
+    case(3):
+      bp = rtk::NormalizedJosephBackProjectionImageFilter<THessians,THessians>::New();
+      break;
+    case(4):
+    #ifdef RTK_USE_CUDA
+      // The current CUDA implementation only allows one component
+//      bp = rtk::CudaRayCastBackProjectionImageFilter::New();
+    #else
+      itkGenericExceptionMacro(<< "The program has not been compiled with cuda option");
+    #endif
+      break;
+    default:
+      itkGenericExceptionMacro(<< "Unhandled --bp value.");
+    }
+  return bp;
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+void
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::GenerateInputRequestedRegion()
+{
+  // Input 0 is the volume we update
+  typename Superclass::InputImagePointer inputPtr0 =
+          const_cast< TOutputImage * >( this->GetInput(0) );
+  if ( !inputPtr0 )
+      return;
+  inputPtr0->SetRequestedRegion( this->GetOutput()->GetRequestedRegion() );
+
+  // Input 1 is the photon counts
+  typename Superclass::InputImagePointer  inputPtr1 =
+          const_cast< TOutputImage * >( this->GetInput(1) );
+  if ( !inputPtr1 )
+      return;
+  inputPtr1->SetRequestedRegion( inputPtr1->GetLargestPossibleRegion() );
+
+  // Input 2 is the weights map on projections, either user-defined or filled with ones (default)
+  typename Superclass::InputImagePointer  inputPtr2 =
+          const_cast< TOutputImage * >( this->GetInput(2) );
+  if ( !inputPtr2 )
+      return;
+  inputPtr2->SetRequestedRegion( inputPtr2->GetLargestPossibleRegion() );
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+void
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::GenerateOutputInformation()
+{
+  // Set runtime connections. Links with the forward and back projection filters should be set here,
+  // since those filters are not instantiated by the constructor, but by
+  // a call to SetForwardProjectionFilter() and SetBackProjectionFilter()
+  m_ForwardProjectionFilter->SetInput(0, m_ProjectionsSource->GetOutput());
+  m_ForwardProjectionFilter->SetInput(1, this->GetInputMaterialVolumes());
+
+  m_SingleComponentForwardProjectionFilter->SetInput(0, m_SingleComponentProjectionsSource->GetOutput());
+  m_SingleComponentForwardProjectionFilter->SetInput(1, m_SingleComponentVolumeSource->GetOutput());
+
+  m_WeidingerForward->SetInputMaterialProjections(m_ForwardProjectionFilter->GetOutput());
+  m_WeidingerForward->SetInputPhotonCounts(this->GetInputPhotonCounts());
+  m_WeidingerForward->SetInputSpectrum(this->GetInputSpectrum());
+  m_WeidingerForward->SetInputProjectionsOfOnes(m_SingleComponentForwardProjectionFilter->GetOutput());
+
+  m_GradientsBackProjectionFilter->SetInput(0, m_GradientsSource->GetOutput());
+  m_GradientsBackProjectionFilter->SetInput(1, m_WeidingerForward->GetOutput1());
+
+  m_HessiansBackProjectionFilter->SetInput(0, m_HessiansSource->GetOutput());
+  m_HessiansBackProjectionFilter->SetInput(1, m_WeidingerForward->GetOutput2());
+
+  m_NewtonFilter->SetInputGradient(m_GradientsBackProjectionFilter->GetOutput());
+  m_NewtonFilter->SetInputHessian(m_HessiansBackProjectionFilter->GetOutput());
+
+  m_SubtractFilter->SetInput1(this->GetInputMaterialVolumes());
+  m_SubtractFilter->SetInput2(m_NewtonFilter->GetOutput());
+
+  // Set information for the sources
+  m_SingleComponentProjectionsSource->SetInformationFromImage(this->GetInputPhotonCounts());
+  m_ProjectionsSource->SetInformationFromImage(this->GetInputPhotonCounts());
+  m_SingleComponentVolumeSource->SetInformationFromImage(this->GetInputMaterialVolumes());
+  m_GradientsSource->SetInformationFromImage(this->GetInputMaterialVolumes());
+  m_HessiansSource->SetInformationFromImage(this->GetInputMaterialVolumes());
+
+  // For the same reason, set geometry now
+  m_ForwardProjectionFilter->SetGeometry(this->m_Geometry);
+  m_SingleComponentForwardProjectionFilter->SetGeometry(this->m_Geometry);
+  m_GradientsBackProjectionFilter->SetGeometry(this->m_Geometry.GetPointer());
+  m_HessiansBackProjectionFilter->SetGeometry(this->m_Geometry.GetPointer());
+
+  // Set memory management parameters
+
+
+  // Have the last filter calculate its output information
+  m_SubtractFilter->UpdateOutputInformation();
+
+  // Copy it as the output information of the composite filter
+  this->GetOutput()->CopyInformation( m_SubtractFilter->GetOutput() );
+}
+
+template< class TOutputImage, class TPhotonCounts, class TSpectrum>
+void
+MechlemOneStepSpectralReconstructionFilter< TOutputImage, TPhotonCounts, TSpectrum>
+::GenerateData()
+{
+typename TOutputImage::Pointer pimg;
+
+for(unsigned int iter = 0; iter < m_NumberOfIterations; iter++)
+  {
+  if (iter>0)
+    {
+    pimg = m_SubtractFilter->GetOutput();
+    pimg->DisconnectPipeline();
+    m_SubtractFilter->SetInput1(pimg);
+    m_ForwardProjectionFilter->SetInput(1, pimg);
+    }
+  m_SubtractFilter->Update();
+
+  }
+  this->GraftOutput( m_SubtractFilter->GetOutput() );
+}
+
+}// end namespace
+
+
+#endif
