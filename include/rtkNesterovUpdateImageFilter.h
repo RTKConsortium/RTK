990e82a06e03cb6f935e6317c59f630aa6994a62
Cyril Mory <cyril.mory@gmail.com>
2018-05-03 13:00:27 +0200

    BUG: Fixed mistake in the definition of superclass in rtkNesterovUpdateImageFilter

--- include/rtkNesterovUpdateImageFilter.h
+++ include/rtkNesterovUpdateImageFilter.h
@@ -49,7 +49,7 @@ public:
   itkNewMacro(Self)
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(NesterovUpdateImageFilter, itk::ImageToImageFilter)
+  itkTypeMacro(NesterovUpdateImageFilter, itk::InPlaceImageFilter)
 
   /** Get and Set macro*/
   itkGetMacro(NumberOfIterations, int)
