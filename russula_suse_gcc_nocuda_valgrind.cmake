54e23dc01412fe12b54af84c5fdeda520fe0ac40
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2018-08-30 15:05:32 +0200

    FIX: dynamic analysis on CREATIS cluster and russula

--- russula_suse_gcc_nocuda_valgrind.cmake
+++ russula_suse_gcc_nocuda_valgrind.cmake
@@ -1,19 +1,18 @@
 # client maintainer: simon.rit@creatis.insa-lyon.fr
-set(CTEST_SITE "linux15.dg.creatis.insa-lyon.fr")
-set(CTEST_BUILD_NAME "Linux-64bit-gcc-nocuda-system-itk4-valgrind")
+set(CTEST_SITE "russula.clb")
+set(CTEST_BUILD_NAME "Linux-64bit-gcc-nocuda-valgrind")
 set(CTEST_BUILD_CONFIGURATION RelWithDebInfo)
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(CTEST_DASHBOARD_ROOT "/tmp/RTK_dashboard")
 set(dashboard_binary_name "RTK_lin64_gcc_nocuda_system_itk4_valgrind")
-set(ENV{ITK_DIR} "/home/srit/src/itk/lin64-dg-debug")
-set(CTEST_BUILD_FLAGS -j12)
+set(ENV{ITK_DIR} "/home/srit/src/itk/lin64-RelWithDebInfo")
+set(CTEST_BUILD_FLAGS -j16)
 
 set(ENV{VALGRIND_LIB} "/usr/lib64/valgrind")
 set(CTEST_MEMORYCHECK_COMMAND /usr/bin/valgrind)
-set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--gen-suppressions=all --child-silent-after-fork=yes -q --leak-check=yes --show-reachable=yes --workaround-gcc296-bugs=yes --num-callers=50 -v")
+set(CTEST_MEMORYCHECK_COMMAND_OPTIONS "--gen-suppressions=all --child-silent-after-fork=yes -q --leak-check=yes --show-reachable=yes --num-callers=50 -v")
 set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE "${CTEST_SCRIPT_DIRECTORY}/RTK.supp")
 set(CTEST_CUSTOM_MEMCHECK_IGNORE "RTKInDoxygenGroup")
-set(CONFIGURE_OPTIONS -DRTK_USE_CUDA=FALSE
+set(CONFIGURE_OPTIONS -DRTK_USE_CUDA=FALSE 
                       -DFAST_TESTS_NO_CHECKS=TRUE)
 set(dashboard_do_memcheck true)
 
