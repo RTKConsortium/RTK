c02ebe12514de42ff148220d74b57029d8a0d468
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2018-11-30 12:09:24 +0100

    Updated CI configuration of Russula (linux Suse)

--- russula_lin64.sh
+++ russula_lin64.sh
@@ -1,6 +1,12 @@
 #!/bin/bash -x
 export CXXFLAGS="-fPIC -std=c++11"
 ctest -S /home/srit/src/rtk/rtk-dashboard/russula_suse_gcc.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/russula_suse_gcc_shared_itk-v4.12.0.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/russula_suse_gcc_itk-v4.12.0.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/russula_suse_gcc_shared_itk-v4.13.1_nocuda.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/russula_suse_gcc_shared_itk-v4.13.1.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/russula_suse_gcc_itk-v4.13.1.cmake -V
+ctest -S /home/srit/src/rtk/rtk-dashboard/russula_suse_gcc_shared.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/russula_suse_gcc_debug.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/russula_style.cmake -V
 ctest -S /home/srit/src/rtk/rtk-dashboard/russula_doxygen.cmake -V
---
+++ russula_suse_gcc_shared.cmake
@@ -0,0 +1,11 @@
+# client maintainer: simon.rit@creatis.insa-lyon.fr
+set(CTEST_SITE "russula.clb")
+set(CTEST_BUILD_NAME "Linux-64bit-gcc-shared")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(dashboard_binary_name "RTK_lin64_gcc_shared")
+set(ENV{ITK_DIR} "/home/srit/src/itk/lin64-SharedLibs-Release")
+set(CTEST_BUILD_FLAGS -j16)
+set(ENV{CUDA_BIN_PATH} "/home/srit/Download/cuda80/bin")
+include("${CTEST_SCRIPT_DIRECTORY}/rtk_common.cmake")
+
