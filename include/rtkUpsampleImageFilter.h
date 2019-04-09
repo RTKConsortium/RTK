RTKConsortium/RTK

commit 039cc1f4d5fb11b69042065ffb091910f7f87dc9

FIX: use of TBB revealed bugs as pointed out in issue #238

include/rtkUpsampleImageFilter.h
@@ -19,7 +19,8 @@
 #ifndef rtkUpsampleImageFilter_h
 #define rtkUpsampleImageFilter_h
 
-#include "itkImageToImageFilter.h"
+#include <itkImageToImageFilter.h>
+#include <itkImageRegionSplitterDirection.h>
 
 namespace rtk
 {
@@ -130,19 +131,20 @@ protected:
    *
    * \sa ImageToImageFilter::ThreadedGenerateData(),
    *     ImageToImageFilter::GenerateData() */
-//  void BeforeThreadedGenerateData();
 #if ITK_VERSION_MAJOR<5
   void ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread, itk::ThreadIdType itkNotUsed(threadId)) override;
 #else
   void DynamicThreadedGenerateData(const OutputImageRegionType& outputRegionForThread) override;
 #endif
-//  void AfterThreadedGenerateData();
 
 private:
   unsigned int                      m_Factors[ImageDimension];
   unsigned int                      m_Order;
   typename TOutputImage::SizeType   m_OutputSize;
   typename TOutputImage::IndexType  m_OutputIndex;
+
+  const itk::ImageRegionSplitterBase* GetImageRegionSplitter(void) const override;
+  itk::ImageRegionSplitterDirection::Pointer m_Splitter;
 };
 
 

include/rtkUpsampleImageFilter.hxx
@@ -34,10 +34,17 @@ template <class TInputImage, class TOutputImage>
 UpsampleImageFilter<TInputImage,TOutputImage>
 ::UpsampleImageFilter()
 {
+#if ITK_VERSION_MAJOR>4
+  this->DynamicMultiThreadingOff();
+#endif
   this->SetNumberOfRequiredInputs(1);
   this->m_Order = 0;
   this->m_OutputSize.Fill(0);
   this->m_OutputIndex.Fill(0);
+
+  // Set the direction along which the output requested region should NOT be split
+  m_Splitter = itk::ImageRegionSplitterDirection::New();
+  m_Splitter->SetDirection(0);
 }
 
 template <class TInputImage, class TOutputImage>
@@ -254,6 +261,13 @@ UpsampleImageFilter<TInputImage,TOutputImage>
   outputPtr->SetOrigin(inputPtr->GetOrigin());
 }
 
+template <class TInputImage, class TOutputImage>
+const itk::ImageRegionSplitterBase*
+UpsampleImageFilter<TInputImage,TOutputImage>
+::GetImageRegionSplitter(void) const
+{
+  return m_Splitter;
+}
 } // end namespace rtk
 
 #endif

include/rtkDisplacedDetectorForOffsetFieldOfViewImageFilter.hxx
@@ -127,15 +127,19 @@ DisplacedDetectorForOffsetFieldOfViewImageFilter<TInputImage, TOutputImage>
 #endif
 {
   // Compute overlap between input and output
+  itk::ImageRegionIterator<OutputImageType> itOut(this->GetOutput(), outputRegionForThread);
   OutputImageRegionType overlapRegion = outputRegionForThread;
-
-  overlapRegion.Crop(this->GetInput()->GetLargestPossibleRegion() );
-
-  // Input / ouput iterators
+  if( !overlapRegion.Crop( this->GetInput()->GetLargestPossibleRegion() ) )
+    {
+    // No overlap, set output region to 0
+    while(!itOut.IsAtEnd() )
+      {
+      itOut.Set( 0. );
+      ++itOut;
+      }
+    return;
+    }
   itk::ImageRegionConstIterator<InputImageType> itIn(this->GetInput(), overlapRegion);
-  itk::ImageRegionIterator<OutputImageType>     itOut(this->GetOutput(), outputRegionForThread);
-  itIn.GoToBegin();
-  itOut.GoToBegin();
 
   // Not displaced, nothing to do
   if( this->GetInput()->GetLargestPossibleRegion().GetSize()[0] ==

include/rtkDisplacedDetectorImageFilter.hxx
@@ -191,15 +191,19 @@ DisplacedDetectorImageFilter<TInputImage, TOutputImage>
   itk::NumericTraits<typename TOutputImage::PixelType>::SetLength(pix, this->GetInput()->GetNumberOfComponentsPerPixel());
 
   // Compute overlap between input and output
+  itk::ImageRegionIterator<OutputImageType> itOut(this->GetOutput(), outputRegionForThread);
   OutputImageRegionType overlapRegion = outputRegionForThread;
-
-  overlapRegion.Crop(this->GetInput()->GetLargestPossibleRegion() );
-
-  // Input / ouput iterators
+  if( !overlapRegion.Crop( this->GetInput()->GetLargestPossibleRegion() ) )
+    {
+    // No overlap, set output region to 0
+    while(!itOut.IsAtEnd() )
+      {
+      itOut.Set( 0. );
+      ++itOut;
+      }
+    return;
+    }
   itk::ImageRegionConstIterator<InputImageType> itIn(this->GetInput(), overlapRegion);
-  itk::ImageRegionIterator<OutputImageType>     itOut(this->GetOutput(), outputRegionForThread);
-  itIn.GoToBegin();
-  itOut.GoToBegin();
 
   // Not displaced, nothing to do
   if( (fabs(m_InferiorCorner+m_SuperiorCorner) < 0.1*fabs(m_SuperiorCorner-m_InferiorCorner)) || m_Disable)
@@ -235,7 +239,6 @@ DisplacedDetectorImageFilter<TInputImage, TOutputImage>
   typename itk::ImageRegionIteratorWithIndex<WeightImageType> itWeights(weights, weights->GetLargestPossibleRegion() );
 
   double theta = std::min(-1*m_InferiorCorner, m_SuperiorCorner);
-
   for(unsigned int k=0; k<overlapRegion.GetSize(2); k++)
     {
     // Prepare weights for current slice (depends on ProjectionOffsetsX)

include/rtkReconstructImageFilter.h
@@ -174,11 +174,7 @@ public:
      * VerifyInputInformation method has to be reimplemented.
      */
 #if ITK_VERSION_MAJOR<5
-#if ITK_VERSION_MAJOR<5
     void VerifyInputInformation() override {}
-#else
-    void VerifyInputInformation() const override {}
-#endif
 #else
     void VerifyInputInformation() const override {}
 #endif

commit 4412d40025761cd5be4e34385143c11d11394206

FIX: with DynamicMultiThreadingOff, use ThreadedGenerateData
Bug introduced by 039cc1f, not sure why CI did not catch it...

include/rtkUpsampleImageFilter.h
@@ -131,11 +131,7 @@ protected:
    *
    * \sa ImageToImageFilter::ThreadedGenerateData(),
    *     ImageToImageFilter::GenerateData() */
-#if ITK_VERSION_MAJOR<5
   void ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread, itk::ThreadIdType itkNotUsed(threadId)) override;
-#else
-  void DynamicThreadedGenerateData(const OutputImageRegionType& outputRegionForThread) override;
-#endif
 
 private:
   unsigned int                      m_Factors[ImageDimension];

include/rtkUpsampleImageFilter.hxx
@@ -89,11 +89,7 @@ UpsampleImageFilter<TInputImage,TOutputImage>
 template <class TInputImage, class TOutputImage>
 void
 UpsampleImageFilter<TInputImage,TOutputImage>
-#if ITK_VERSION_MAJOR<5
 ::ThreadedGenerateData(const OutputImageRegionType& outputRegionForThread, itk::ThreadIdType itkNotUsed(threadId))
-#else
-::DynamicThreadedGenerateData(const OutputImageRegionType& outputRegionForThread)
-#endif
 {
   //Get the input and output pointers
   InputImageConstPointer  inputPtr    = this->GetInput();