cbab17c022b6a1e9a5fcb9fc17de0f366a1ad9b6
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2021-06-23 23:34:44 +0200

    Change cmake version

--- russula.sh
+++ russula.sh
@@ -3,6 +3,7 @@ export CXXFLAGS="-fPIC -std=c++11"
 export CUDA_BIN_PATH=/home/srit/Downloads/cuda102/bin
 export CUDA_LIB_PATH=/home/srit/Downloads/cuda102/lib64
 export LD_LIBRARY_PATH=$CUDA_LIB_PATH:$LD_LIBRARY_PATH
+export PATH=/home/srit/Downloads/cmake-3.20.4-linux-x86_64/bin:$PATH
 
 ctest -R "(rtk|RTK)" -S /home/srit/src/rtk/rtk-dashboard/russula_build_rtk_in_itk.cmake -V
 ctest -R "(rtk|RTK)" -S /home/srit/src/rtk/rtk-dashboard/russula_tbb.cmake -V
