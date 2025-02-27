3641b31b437921d6ae9e9a0ecf7ffc0b150986fa
Axel Garcia <axel.garcia@creatis.insa-lyon.fr>
2025-02-27 16:01:59 +0100

    ENH: Rename GetCudaImageFromImage and move it to rtkExtra

--- wrapping/rtkExtra.py
+++ wrapping/rtkExtra.py
@@ -20,3 +20,15 @@ def ReadGeometry(filename):
 
     # Return the geometry object
     return reader.GetOutputObject()
+
+# Convert an ITK image to a CUDA image if CUDA is available.
+def CudaImageFromImage(img):
+    if hasattr(itk, 'CudaImage'):
+        img.Update()  # Ensure the image is up to date
+        cuda_img = itk.CudaImage[itk.itkCType.GetCTypeForDType(img.dtype), img.ndim].New()
+        cuda_img.SetPixelContainer(img.GetPixelContainer())
+        cuda_img.CopyInformation(img)
+        cuda_img.SetBufferedRegion(img.GetBufferedRegion())
+        cuda_img.SetRequestedRegion(img.GetRequestedRegion())
+        return cuda_img
+    return img  # Return the original image if CUDA is not available
