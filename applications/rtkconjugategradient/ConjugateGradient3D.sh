7700dbfd7723fbae5b3d67b9f134a7bff3000d62
Axel Garcia <axel.garcia@creatis.insa-lyon.fr>
2025-03-13 10:12:18 +0100

    DOC: Fix output images to correspond to the scripts

--- applications/rtkconjugategradient/ConjugateGradient3D.sh
+++ applications/rtkconjugategradient/ConjugateGradient3D.sh
@@ -6,16 +6,7 @@
  rtkprojectshepploganphantom -g geometry.xml -o projections.mha --spacing 2 --dimension 256
 
  # Reconstruct
- rtkconjugategradient -p . -r projections.mha -o 3dcg.mha -g geometry.xml --spacing 2 --dimension 256 -n 20
+ rtkconjugategradient -p . -r projections.mha -o 3dcg.mha -g geometry.xml --spacing 2 --dimension 256 -n 10
 
  # Create a reference volume for comparison
  rtkdrawshepploganphantom --spacing 2 --dimension 256 -o ref.mha
-
- # Perform least squares reconstruction
- rtkconjugategradient -p . -r noisyLineIntegrals.mha -o LeastSquares.mha -g geom.xml -n 20
-
-#  # Perform weighted least squares reconstruction
-#  rtkconjugategradient -p . -r noisyLineIntegrals.mha -o WeightedLeastSquares.mha -g geom.xml -w weightsmap.mha -n 20
-
-#  # Perform preconditioned conjugate gradient reconstruction with weighted least squares cost function
-#  rtkconjugategradient -p . -r noisyLineIntegrals.mha -o WeightedLeastSquares.mha -g geom.xml -w weightsmap.mha -n 20 --preconditioned
