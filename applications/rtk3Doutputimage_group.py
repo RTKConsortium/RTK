e64daf8d8b3b97ca1f85ba4e6637b8852b78075c
Simon Rit <simon.rit@creatis.insa-lyon.fr>
2025-04-24 15:40:47 +0200

    STYLE: Enforce black formatting for toolkit
    Following InsightSoftwareConsortium/ITK#5013. Done with:
    find -name '*py' -exec dos2unix {} \;
    find -name '*py' -exec black {} \;
    find -name '*py' -exec git add {} \;

--- applications/rtk3Doutputimage_group.py
+++ applications/rtk3Doutputimage_group.py
@@ -2,61 +2,78 @@ import itk
 from itk import RTK as rtk
 
 __all__ = [
-    'add_rtk3Doutputimage_group',
-    'SetConstantImageSourceFromArgParse',
+    "add_rtk3Doutputimage_group",
+    "SetConstantImageSourceFromArgParse",
 ]
 
 # Mimicks rtk3Doutputimage_section.ggo
 def add_rtk3Doutputimage_group(parser):
-  rtk3Doutputimage_group = parser.add_argument_group('Output 3D image properties')
-  rtk3Doutputimage_group.add_argument('--origin', help='Origin (default=centered)', type=rtk.comma_separated_args(float))
-  rtk3Doutputimage_group.add_argument('--dimension', help='Dimension', type=rtk.comma_separated_args(int), default=[256])
-  rtk3Doutputimage_group.add_argument('--spacing', help='Spacing', type=rtk.comma_separated_args(float), default=[1])
-  rtk3Doutputimage_group.add_argument('--direction', help='Direction', type=rtk.comma_separated_args(float))
-  rtk3Doutputimage_group.add_argument('--like', help='Copy information from this image (origin, dimension, spacing, direction)')
+    rtk3Doutputimage_group = parser.add_argument_group("Output 3D image properties")
+    rtk3Doutputimage_group.add_argument(
+        "--origin",
+        help="Origin (default=centered)",
+        type=rtk.comma_separated_args(float),
+    )
+    rtk3Doutputimage_group.add_argument(
+        "--dimension",
+        help="Dimension",
+        type=rtk.comma_separated_args(int),
+        default=[256],
+    )
+    rtk3Doutputimage_group.add_argument(
+        "--spacing", help="Spacing", type=rtk.comma_separated_args(float), default=[1]
+    )
+    rtk3Doutputimage_group.add_argument(
+        "--direction", help="Direction", type=rtk.comma_separated_args(float)
+    )
+    rtk3Doutputimage_group.add_argument(
+        "--like",
+        help="Copy information from this image (origin, dimension, spacing, direction)",
+    )
+
 
 # Mimicks SetConstantImageSourceFromGgo
 def SetConstantImageSourceFromArgParse(source, args_info):
-  ImageType = type(source.GetOutput())
+    ImageType = type(source.GetOutput())
 
-  Dimension = ImageType.GetImageDimension()
+    Dimension = ImageType.GetImageDimension()
 
-  imageDimension = itk.Size[Dimension]()
-  imageDimension.Fill(args_info.dimension[0])
-  for i in range(min(len(args_info.dimension), Dimension)):
-    imageDimension[i] = args_info.dimension[i]
+    imageDimension = itk.Size[Dimension]()
+    imageDimension.Fill(args_info.dimension[0])
+    for i in range(min(len(args_info.dimension), Dimension)):
+        imageDimension[i] = args_info.dimension[i]
 
-  imageSpacing = itk.Vector[itk.D, Dimension]()
-  imageSpacing.Fill(args_info.spacing[0])
-  for i in range(min(len(args_info.spacing), Dimension)):
-    imageSpacing[i] = args_info.spacing[i]
+    imageSpacing = itk.Vector[itk.D, Dimension]()
+    imageSpacing.Fill(args_info.spacing[0])
+    for i in range(min(len(args_info.spacing), Dimension)):
+        imageSpacing[i] = args_info.spacing[i]
 
-  imageOrigin = itk.Point[itk.D, Dimension]()
-  for i in range(Dimension):
-    imageOrigin[i] = imageSpacing[i] * (imageDimension[i] - 1) * -0.5
-  if args_info.origin is not None:
-    for i in range(min(len(args_info.origin), Dimension)):
-      imageOrigin[i] = args_info.origin[i]
-
-  imageDirection = source.GetOutput().GetDirection()
-  if args_info.direction is not None:
+    imageOrigin = itk.Point[itk.D, Dimension]()
     for i in range(Dimension):
-      for j in range(Dimension):
-        imageDirection[i][j] = args_info.direction[i * Dimension + j]
+        imageOrigin[i] = imageSpacing[i] * (imageDimension[i] - 1) * -0.5
+    if args_info.origin is not None:
+        for i in range(min(len(args_info.origin), Dimension)):
+            imageOrigin[i] = args_info.origin[i]
 
-  source.SetOrigin(imageOrigin)
-  source.SetSpacing(imageSpacing)
-  source.SetDirection(imageDirection)
-  source.SetSize(imageDimension)
-  source.SetConstant(0.)
+    imageDirection = source.GetOutput().GetDirection()
+    if args_info.direction is not None:
+        for i in range(Dimension):
+            for j in range(Dimension):
+                imageDirection[i][j] = args_info.direction[i * Dimension + j]
 
-  # Copy output image information from an existing file, if requested
-  # Overwrites parameters given in command line, if any
-  if args_info.like is not None:
-    LikeReaderType = itk.ImageFileReader[ImageType]
-    likeReader = LikeReaderType.New()
-    likeReader.SetFileName(args_info.like)
-    likeReader.UpdateOutputInformation()
-    source.SetInformationFromImage(likeReader.GetOutput())
+    source.SetOrigin(imageOrigin)
+    source.SetSpacing(imageSpacing)
+    source.SetDirection(imageDirection)
+    source.SetSize(imageDimension)
+    source.SetConstant(0.0)
 
-  source.UpdateOutputInformation()
+    # Copy output image information from an existing file, if requested
+    # Overwrites parameters given in command line, if any
+    if args_info.like is not None:
+        LikeReaderType = itk.ImageFileReader[ImageType]
+        likeReader = LikeReaderType.New()
+        likeReader.SetFileName(args_info.like)
+        likeReader.UpdateOutputInformation()
+        source.SetInformationFromImage(likeReader.GetOutput())
+
+    source.UpdateOutputInformation()
--- examples/FirstReconstruction/FirstReconstruction.py
+++ examples/FirstReconstruction/FirstReconstruction.py
@@ -3,23 +3,23 @@ import sys
 import itk
 from itk import RTK as rtk
 
-if len ( sys.argv ) < 3:
-  print( "Usage: FirstReconstruction <outputimage> <outputgeometry>" )
-  sys.exit ( 1 )
+if len(sys.argv) < 3:
+    print("Usage: FirstReconstruction <outputimage> <outputgeometry>")
+    sys.exit(1)
 
 # Defines the image type
-ImageType = itk.Image[itk.F,3]
+ImageType = itk.Image[itk.F, 3]
 
 # Defines the RTK geometry object
 geometry = rtk.ThreeDCircularProjectionGeometry.New()
 numberOfProjections = 360
-firstAngle = 0.
-angularArc = 360.
-sid = 600 # source to isocenter distance
-sdd = 1200 # source to detector distance
-for x in range(0,numberOfProjections):
-  angle = firstAngle + x * angularArc / numberOfProjections
-  geometry.AddProjection(sid,sdd,angle)
+firstAngle = 0.0
+angularArc = 360.0
+sid = 600  # source to isocenter distance
+sdd = 1200  # source to detector distance
+for x in range(0, numberOfProjections):
+    angle = firstAngle + x * angularArc / numberOfProjections
+    geometry.AddProjection(sid, sdd, angle)
 
 # Writing the geometry to disk
 rtk.write_geometry(geometry, sys.argv[2])
@@ -27,35 +27,35 @@ rtk.write_geometry(geometry, sys.argv[2])
 # Create a stack of empty projection images
 ConstantImageSourceType = rtk.ConstantImageSource[ImageType]
 constantImageSource = ConstantImageSourceType.New()
-origin = [ -127, -127, 0. ]
-sizeOutput = [ 128, 128,  numberOfProjections ]
-spacing = [ 2.0, 2.0, 2.0 ]
-constantImageSource.SetOrigin( origin )
-constantImageSource.SetSpacing( spacing )
-constantImageSource.SetSize( sizeOutput )
-constantImageSource.SetConstant(0.)
+origin = [-127, -127, 0.0]
+sizeOutput = [128, 128, numberOfProjections]
+spacing = [2.0, 2.0, 2.0]
+constantImageSource.SetOrigin(origin)
+constantImageSource.SetSpacing(spacing)
+constantImageSource.SetSize(sizeOutput)
+constantImageSource.SetConstant(0.0)
 
 REIType = rtk.RayEllipsoidIntersectionImageFilter[ImageType, ImageType]
 rei = REIType.New()
-semiprincipalaxis = [ 50, 50, 50]
-center = [ 0, 0, 10]
+semiprincipalaxis = [50, 50, 50]
+center = [0, 0, 10]
 # Set GrayScale value, axes, center...
 rei.SetDensity(2)
 rei.SetAngle(0)
 rei.SetCenter(center)
 rei.SetAxis(semiprincipalaxis)
-rei.SetGeometry( geometry )
+rei.SetGeometry(geometry)
 rei.SetInput(constantImageSource.GetOutput())
 
 # Create reconstructed image
 constantImageSource2 = ConstantImageSourceType.New()
-sizeOutput = [ 128, 128, 128 ]
-origin = [ -63.5, -63.5, -63.5 ]
-spacing = [ 1.0, 1.0, 1.0 ]
-constantImageSource2.SetOrigin( origin )
-constantImageSource2.SetSpacing( spacing )
-constantImageSource2.SetSize( sizeOutput )
-constantImageSource2.SetConstant(0.)
+sizeOutput = [128, 128, 128]
+origin = [-63.5, -63.5, -63.5]
+spacing = [1.0, 1.0, 1.0]
+constantImageSource2.SetOrigin(origin)
+constantImageSource2.SetSpacing(spacing)
+constantImageSource2.SetSize(sizeOutput)
+constantImageSource2.SetConstant(0.0)
 
 # FDK reconstruction
 print("Reconstructing...")
@@ -83,4 +83,3 @@ writer.SetInput(fieldofview.GetOutput())
 writer.Update()
 
 print("Done!")
-
