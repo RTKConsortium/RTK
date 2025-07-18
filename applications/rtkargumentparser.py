e9c9c9b50c972d122d20dbecee9698437562e135
Aurélien Coussat <aurelien.coussat@creatis.insa-lyon.fr>
2025-07-18 15:31:10 +0200

    WIP: Modify the formatter to display the version at the top

--- applications/rtkargumentparser.py
+++ applications/rtkargumentparser.py
@@ -1,15 +1,23 @@
 import argparse
+from gettext import gettext as _
 from itk import RTK as rtk
 
 __all__ = [
     "rtk_argument_parser"
 ]
 
+class RTKHelpFormatter(argparse.ArgumentDefaultsHelpFormatter):
+    def _format_usage(self, usage, actions, groups, prefix=None):
+        if prefix is None:
+            prefix = _(rtk.version() + "\n\nusage: ")
+        return super()._format_usage(usage, actions, groups, prefix)
+
 def rtk_argument_parser(desc):
+
     parser = argparse.ArgumentParser(
         description=desc,
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
-        epilog=rtk.version()
+        formatter_class=RTKHelpFormatter
     )
     parser.add_argument('-V', '--version', action='version', version=rtk.version())
+
     return parser
