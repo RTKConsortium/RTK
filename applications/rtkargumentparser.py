02006f02b8a14958e79151736f7b13baf01f92c9
Aurélien Coussat <aurelien.coussat@creatis.insa-lyon.fr>
2025-07-18 15:05:04 +0200

    WIP: Create an option --version

--- applications/rtkargumentparser.py
+++ applications/rtkargumentparser.py
@@ -6,8 +6,10 @@ __all__ = [
 ]
 
 def rtk_argument_parser(desc):
-    return argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         description=desc,
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
         epilog=rtk.version()
     )
+    parser.add_argument('-V', '--version', action='version', version=rtk.version())
+    return parser
