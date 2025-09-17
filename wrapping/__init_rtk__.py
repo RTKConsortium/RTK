07792d4e02041554bd29188fdc6a1130c320dd67
Axel Garcia <axel.garcia1997@gmail.com>
2025-09-17 10:49:31 +0200

    ENH: Add rtkmaskcollimation python application

--- wrapping/__init_rtk__.py
+++ wrapping/__init_rtk__.py
@@ -43,6 +43,7 @@ _app_modules = [
     "rtki0estimation",
     "rtkimagxgeometry",
     "rtklagcorrection",
+    "rtkmaskcollimation",
     "rtkorageometry",
     "rtkprojectgeometricphantom",
     "rtkprojections",
