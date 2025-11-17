3eed32fcd26cbb655ab6eecd1a9d7196a51b9889
Axel Garcia <axel.garcia1997@gmail.com>
2025-11-17 11:30:25 +0100

    ENH: Add rtkfourdrooster python application

--- wrapping/__init_rtk__.py
+++ wrapping/__init_rtk__.py
@@ -40,6 +40,7 @@ _app_modules = [
     "rtkforwardprojections",
     "rtkfourdconjugategradient",
     "rtkfourdfdk",
+    "rtkfourdrooster",
     "rtki0estimation",
     "rtkimagxgeometry",
     "rtkmaskcollimation",
