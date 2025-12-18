582c42b3b13dc72d678bab0db57d002f108a9eb2
Axel Garcia <axel.garcia1997@gmail.com>
2025-12-18 10:11:45 +0100

    ENH: Add rtktotalnuclearvariationdenoising python application

--- wrapping/__init_rtk__.py
+++ wrapping/__init_rtk__.py
@@ -58,6 +58,7 @@ _app_modules = [
     "rtksart",
     "rtksimulatedgeometry",
     "rtksubselect",
+    "rtktotalnuclearvariationdenoising",
     "rtktotalvariationdenoising",
     "rtkvarianobigeometry",
     "rtkwaveletsdenoising",
