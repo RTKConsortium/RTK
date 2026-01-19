52d617bd7b7b222cb8710c82294f650beb6dc258
Cyril Mory <cyril.mory@gmail.com>
2026-01-19 07:26:22 +0100

    ENH: Move 4D example data generation to a separate example
    Adapt FourDConjugateGradient example to use the example 4D data
    Order the tests so that GenerateFourDData runs first. It saves its
    data in the test/ folder, where tests read

--- examples/FourDConjugateGradient/FourDConjugateGradient.cxx
+++ examples/FourDConjugateGradient/FourDConjugateGradient.cxx
@@ -1,9 +1,9 @@
-// #include "rtkThreeDCircularProjectionGeometryXMLFile.h"
 #include "rtkFourDConjugateGradientConeBeamReconstructionFilter.h"
 #include "rtkIterationCommands.h"
 #include "rtkSignalToInterpolationWeights.h"
 #include "rtkReorderProjectionsImageFilter.h"
-#include "../../test/rtkFourDTestHelper.h"
+#include "rtkProjectionsReader.h"
+#include "rtkThreeDCircularProjectionGeometryXMLFileReader.h"
 
 #ifdef RTK_USE_CUDA
 #  include <itkCudaImage.h>
@@ -25,24 +25,47 @@ main(int argc, char * argv[])
   using VolumeType = itk::Image<OutputPixelType, 3>;
 #endif
 
-  auto data = rtk::GenerateFourDTestData<OutputPixelType>(FAST_TESTS_NO_CHECKS);
+  // Generate the input volume series, used as initial estimate by 4D conjugate gradient
+  auto fourDOrigin = itk::MakePoint(-63., -31., -63., 0.);
+  auto fourDSpacing = itk::MakeVector(4., 4., 4., 1.);
+  auto fourDSize = itk::MakeSize(32, 16, 32, 8);
+
+  using ConstantFourDSourceType = rtk::ConstantImageSource<VolumeSeriesType>;
+  auto fourDSource = ConstantFourDSourceType::New();
+
+  fourDSource->SetOrigin(fourDOrigin);
+  fourDSource->SetSpacing(fourDSpacing);
+  fourDSource->SetSize(fourDSize);
+  fourDSource->SetConstant(0.);
+  fourDSource->Update();
+
+  // Read geometry, projections and signal
+  rtk::ThreeDCircularProjectionGeometry::Pointer geometry;
+  TRY_AND_EXIT_ON_ITK_EXCEPTION(geometry = rtk::ReadGeometry("four_d_geometry.xml"));
+
+  using ReaderType = rtk::ProjectionsReader<ProjectionStackType>;
+  auto                     projectionsReader = ReaderType::New();
+  std::vector<std::string> fileNames = std::vector<std::string>();
+  fileNames.push_back("four_d_projections.mha");
+  projectionsReader->SetFileNames(fileNames);
+  TRY_AND_EXIT_ON_ITK_EXCEPTION(projectionsReader->Update());
 
   // Re-order geometry and projections
   // In the new order, projections with identical phases are packed together
-  auto reorder = rtk::ReorderProjectionsImageFilter<ProjectionStackType>::New();
-  reorder->SetInput(data.Projections);
-  reorder->SetInputGeometry(data.Geometry);
-  reorder->SetInputSignal(data.Signal);
+  std::vector<double> signal = rtk::ReadSignalFile("four_d_signal.txt");
+  auto                reorder = rtk::ReorderProjectionsImageFilter<ProjectionStackType>::New();
+  reorder->SetInput(projectionsReader->GetOutput());
+  reorder->SetInputGeometry(geometry);
+  reorder->SetInputSignal(signal);
   TRY_AND_EXIT_ON_ITK_EXCEPTION(reorder->Update())
 
   // Release the memory holding the stack of original projections
-  data.Projections->ReleaseData();
+  projectionsReader->GetOutput()->ReleaseData();
 
   // Compute the interpolation weights
   auto signalToInterpolationWeights = rtk::SignalToInterpolationWeights::New();
   signalToInterpolationWeights->SetSignal(reorder->GetOutputSignal());
-  signalToInterpolationWeights->SetNumberOfReconstructedFrames(
-    data.InitialVolumeSeries->GetLargestPossibleRegion().GetSize(3));
+  signalToInterpolationWeights->SetNumberOfReconstructedFrames(fourDSize[3]);
   TRY_AND_EXIT_ON_ITK_EXCEPTION(signalToInterpolationWeights->Update())
 
   // Set the forward and back projection filters to be used
@@ -50,7 +73,7 @@ main(int argc, char * argv[])
     rtk::FourDConjugateGradientConeBeamReconstructionFilter<VolumeSeriesType, ProjectionStackType>;
   auto fourdconjugategradient = ConjugateGradientFilterType::New();
 
-  fourdconjugategradient->SetInputVolumeSeries(data.InitialVolumeSeries);
+  fourdconjugategradient->SetInputVolumeSeries(fourDSource->GetOutput());
   fourdconjugategradient->SetNumberOfIterations(2);
 #ifdef RTK_USE_CUDA
   fourdconjugategradient->SetCudaConjugateGradient(true);
