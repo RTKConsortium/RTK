53212f6ddd24ab2020cc7bad1755bc74ad27ca9b
Axel Garcia <axel.garcia1997@gmail.com>
2026-01-23 11:04:25 +0100

    ENH: Add rtkspectraldenoiseprojections python application

--- wrapping/__init_rtk__.py
+++ wrapping/__init_rtk__.py
@@ -50,6 +50,7 @@ _app_modules = [
     "rtkshowgeometry",
     "rtksart",
     "rtksimulatedgeometry",
+    "rtkspectraldenoiseprojections",
     "rtksubselect",
     "rtktotalvariationdenoising",
     "rtkvarianobigeometry",
