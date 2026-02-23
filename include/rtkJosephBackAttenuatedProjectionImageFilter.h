bd908ae2e9f28c1509b7a918322518558605b12c
Axel Garcia <axel.garcia1997@gmail.com>
2026-02-23 14:13:50 +0100

    STYLE: Remove redondant inline specifier from function declarations
    Using clang-tidy readability-redundant-inline-specifier check

--- include/rtkJosephBackAttenuatedProjectionImageFilter.h
+++ include/rtkJosephBackAttenuatedProjectionImageFilter.h
@@ -54,7 +54,7 @@ public:
     return !(*this != other);
   }
 
-  inline TOutput
+  TOutput
   operator()(const double stepLengthInVoxel, const TCoordinateType weight, const TInput * p, const int i)
   {
     const double w = weight * stepLengthInVoxel;
@@ -107,7 +107,7 @@ public:
     return !(*this != other);
   }
 
-  inline TOutput
+  TOutput
   operator()(const TInput rayValue, const TInput attenuationRay, const VectorType & stepInMM, bool & isNewRay)
   {
     if (isNewRay)
@@ -168,7 +168,7 @@ public:
     return !(*this != other);
   }
 
-  inline void
+  void
   operator()(const TInput &        rayValue,
              TOutput &             output,
              const double          stepLengthInVoxel,
