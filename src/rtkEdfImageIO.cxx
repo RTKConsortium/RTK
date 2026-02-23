dba0adb900559ee01f5bbb59281fd05b5bdfaf18
Axel Garcia <axel.garcia1997@gmail.com>
2026-02-23 15:33:56 +0100

    PERF: Performance-faster-string-find use character
    Using clang-tidy performance-faster-string-find check

--- src/rtkEdfImageIO.cxx
+++ src/rtkEdfImageIO.cxx
@@ -233,7 +233,7 @@ bool
 rtk::EdfImageIO::CanReadFile(const char * FileNameToRead)
 {
   std::string                  filename(FileNameToRead);
-  const std::string::size_type it = filename.find_last_of(".");
+  const std::string::size_type it = filename.find_last_of('.');
   std::string                  fileExt(filename, it + 1, filename.length());
 
   return fileExt == std::string("edf");
