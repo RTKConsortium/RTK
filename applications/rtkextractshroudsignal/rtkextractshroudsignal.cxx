dd28b67bec27568039285ab6a566acb91a5037c6
Axel Garcia <axel.garcia1997@gmail.com>
2026-02-23 16:33:58 +0100

    STYLE: Refactor type casting to use static_cast
    Using clang-tidy -google-readability-casting check

--- applications/rtkextractshroudsignal/rtkextractshroudsignal.cxx
+++ applications/rtkextractshroudsignal/rtkextractshroudsignal.cxx
@@ -95,7 +95,7 @@ main(int argc, char * argv[])
     phase->SetInput(shroudSignal);
     phase->SetMovingAverageSize(args_info.movavg_arg);
     phase->SetUnsharpMaskSize(args_info.unsharp_arg);
-    phase->SetModel((PhaseFilter::ModelType)args_info.model_arg);
+    phase->SetModel(static_cast<PhaseFilter::ModelType>(args_info.model_arg));
     TRY_AND_EXIT_ON_ITK_EXCEPTION(phase->Update())
 
     rtk::WriteSignalToTextFile(phase->GetOutput(), args_info.phase_arg);
