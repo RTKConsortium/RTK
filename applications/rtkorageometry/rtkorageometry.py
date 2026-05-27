ae2de7148d72d58f9c6c3262a2299c3107bae9c5
Axel Garcia <axel.garcia1997@gmail.com>
2026-05-27 17:31:13 +0200

    ENH: Fix python reader applications

--- applications/rtkorageometry/rtkorageometry.py
+++ applications/rtkorageometry/rtkorageometry.py
@@ -7,51 +7,68 @@ from itk import RTK as rtk
 def build_parser():
     # Argument parsing
     parser = rtk.RTKArgumentParser(
-        description="Creates an RTK geometry file from a Varian OBI acquisition."
-    )
-
-    parser.add_argument(
-        "--xml_file", "-x", help="Varian OBI XML information file on projections"
+        description="Creates an RTK geometry file from a sequence of ora.xml files (radART / medPhoton file format)."
     )
     parser.add_argument("--output", "-o", help="Output file name", required=True)
-    parser.add_argument(
-        "--path", "-p", help="Path containing projections", required=True
-    )
-    parser.add_argument(
-        "--regexp", "-r", help="Regular expression to select projection files in path"
-    )
     parser.add_argument(
         "--margin",
         "-m",
         help="Collimation margin (uinf, usup, vinf, vsup)",
         type=float,
         nargs="+",
-        default=0.0,
+        default=[0.0],
+    )
+    parser.add_argument(
+        "--optitrack",
+        help="OptiTrack object ID (unused by default)",
+        type=int,
+        default=-1,
+    )
+    parser.add_argument(
+        "--nsort",
+        help="Numeric sort for regular expression matches",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--submatch",
+        help="Index of the submatch that will be used to sort matches",
+        type=int,
+        default=0,
     )
 
-    # Parse the command line arguments
+    projection_group = parser.add_argument_group("Projections")
+    projection_group.add_argument(
+        "--path", "-p", help="Path containing projections", required=True
+    )
+    projection_group.add_argument(
+        "--regexp",
+        "-r",
+        help="Regular expression to select projection files in path",
+        required=True,
+    )
+    
+
     return parser
 
 
 def process(args: argparse.Namespace):
 
-    margin = args.margin
-    if type(margin) is float:
-        margin = [margin]
-    for i in range(len(margin), 4, 1):
-        margin.append(margin[0])
-    print(margin)
+    margin = itk.Vector[itk.D, 4]()
 
-    names = itk.RegularExpressionSeriesFileNames.New()
-    names.SetDirectory(args.path)
-    names.SetRegularExpression(args.regexp)
+    margin.Fill(args.margin[0])
+    for i in range(min(len(args.margin), 4)):
+        margin[i] = float(args.margin[i])
 
-    reader = rtk.OraGeometryReader.New()
-    reader.SetProjectionsFileNames(names.GetFileNames())
-    reader.SetCollimationMargin(margin)
-    reader.UpdateOutputData()
+    # Create geometry reader
+    fileNames = rtk.GetProjectionsFileNamesFromArgParse(args)
 
-    rtk.write_geometry(reader.GetGeometry(), args.output)
+    oraReader = rtk.OraGeometryReader.New()
+    oraReader.SetProjectionsFileNames(fileNames)
+    oraReader.SetCollimationMargin(margin)
+    oraReader.SetOptiTrackObjectID(getattr(args, "optitrack", -1))
+    oraReader.UpdateOutputData()
+
+    rtk.write_geometry(oraReader.GetGeometry(), args.output)
 
 
 def main(argv=None):
